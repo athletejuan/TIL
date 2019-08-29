e50329adb36a05497022d666e8ac665200196fff
juan <vanillasky84.0627@gmail.com>
2019-08-29 19:33:27 +0900

    Django practice again

---
+++ Web/04_django/PJT02/boards/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('new/', views.new),
+    path('create/', views.create),
+    path('<int:article_id>/', views.detail),
+    path('<int:article_id>/edit/', views.edit),
+    path('<int:article_id>/update/', views.update),
+    path('<int:article_id>/delete/', views.delete),
+]
