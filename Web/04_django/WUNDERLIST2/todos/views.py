9dfb7aeb262f6ef0d39b2f62e16b88dd8db3b3bd
athletejuan <vanillasky84.0627@gmail.com>
2019-09-10 20:38:34 +0900

    Todo List Edit Delete

--- Web/04_django/WUNDERLIST2/todos/views.py
+++ Web/04_django/WUNDERLIST2/todos/views.py
@@ -18,3 +18,25 @@ def create(request):
         
         Todo.objects.create(title=title, due_date=due_date)
         return redirect('/todos')
+
+def edit(request, pk):
+    todo = Todo.objects.get(id=pk)
+    return render(request, 'edit.html', {
+        'todo':todo
+    })
+
+def update(request, pk):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        due_date = request.POST.get('due-date')
+
+        todo = Todo.objects.get(id=pk)
+        todo.title = title
+        todo.due_date = due_date
+        todo.save()
+        return redirect('/todos')
+
+def delete(request, pk):
+    todo = Todo.objects.get(id=pk)
+    todo.delete()
+    return redirect('/todos')
