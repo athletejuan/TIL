2c1419599d6180a43c13e8226fa3163bd9956c97
athletejuan <vanillasky84.0627@gmail.com>
2019-09-11 11:00:55 +0900

    Django request GET

--- Web/04_django/WUNDERLIST/todos/views.py
+++ Web/04_django/WUNDERLIST/todos/views.py
@@ -8,15 +8,18 @@ def index(request):
     }
     return render(request, 'todos/index.html', context)
 
-def new(request):
-    return render(request, 'todos/new.html')
+# def new(request):
+#     return render(request, 'todos/new.html')
 
 def create(request):
-    title = request.POST.get('title')
-    due_date = request.POST.get('due-date')
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        due_date = request.POST.get('due-date')
 
-    Todo.objects.create(title=title, due_date=due_date)
-    return redirect('todos:index')
+        Todo.objects.create(title=title, due_date=due_date)
+        return redirect('todos:index')
+    else:
+        return render(request, 'todos/new.html')
 
 def edit(request, pk):
     todo = Todo.objects.get(id=pk)
@@ -26,15 +29,22 @@ def edit(request, pk):
     return render(request, 'todos/edit.html', context)
 
 def update(request, pk):
-    title = request.POST.get('title')
-    due_date = request.POST.get('due-date')
-    
     todo = Todo.objects.get(id=pk)
-    todo.title = title
-    todo.due_date = due_date
-    todo.save()
-
-    return redirect('todos:index')
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        due_date = request.POST.get('due-date')
+        
+        
+        todo.title = title
+        todo.due_date = due_date
+        todo.save()
+        return redirect('todos:index')
+    else:
+        todo = Todo.objects.get(id=pk)
+        context = {
+            'todo':todo
+        }
+        return render(request, 'todos/edit.html', context)
 
 def delete(request, pk):
     todo = Todo.objects.get(id=pk)
--- Web/04_django/WUNDERLIST3/todos/views.py
+++ Web/04_django/WUNDERLIST3/todos/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Todo
 
-# Create your views here.
+def index(request):
+    todos = Todo.objects.all()[::-1]
+    return render(request, 'index.html',{
+        'todos':todos
+    })
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    title = request.GET.get('title')
+    due_date = request.GET.get('due-date')
+
+    Todo.objects.create(title=title, due_date=due_date)
+    return render(request, 'create.html')
