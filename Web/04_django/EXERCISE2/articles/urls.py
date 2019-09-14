a82b510f499ae99801042dadaa1b9a43131227fd
athletejuan <vanillasky84.0627@gmail.com>
2019-09-14 10:24:34 +0900

    EX2 edit

--- Web/04_django/EXERCISE2/articles/urls.py
+++ Web/04_django/EXERCISE2/articles/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('new/', views.new),
-    path('create/', views.create),
+    # path('create/', views.create),
     path('<int:article_id>/', views.detail),
+    path('<int:article_id>/edit/', views.edit),
     path('<int:article_id>/delete/', views.delete),
 ]
