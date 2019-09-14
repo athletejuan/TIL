d6619282a0be2091349e589f18708bc1142867c7
athletejuan <vanillasky84.0627@gmail.com>
2019-09-15 08:02:07 +0900

    EX3 edit

--- Web/04_django/EXERCISE3/board/views.py
+++ Web/04_django/EXERCISE3/board/views.py
@@ -2,14 +2,21 @@ from django.shortcuts import render, redirect
 from .models import Article
 
 def new(request):
-    return render(request, 'new.html')
+    if request.method == "POST":
+        article = Article()
+        article.title = request.POST.get('input_title')
+        article.content = request.POST.get('input_content')
+        article.save()
+        return redirect(f'/board/{article.id}/')
+    else:
+        return render(request, 'new.html')
 
-def create(request):
-    article = Article()
-    article.title = request.GET.get('input_title')
-    article.content = request.GET.get('input_content')
-    article.save()
-    return redirect(f'/board/{article.id}/')
+# def create(request):
+#     article = Article()
+#     article.title = request.GET.get('input_title')
+#     article.content = request.GET.get('input_content')
+#     article.save()
+#     return redirect(f'/board/{article.id}/')
 
 def index(request):
     articles = Article.objects.all()
@@ -22,6 +29,18 @@ def detail(request, article_id):
     return render(request, 'detail.html', {
         'article':article,
     })
+
+def edit(request, article_id):
+    article = Article.objects.get(id=article_id)
+    if request.method == "POST":
+        article.title = request.POST.get('input_title')
+        article.content = request.POST.get('input_content')
+        article.save()
+        return redirect('/board/')
+    else:
+        return render(request, 'edit.html', {
+            'article':article
+        })
 
 def delete(request, article_id):
     article = Article.objects.get(id=article_id)
