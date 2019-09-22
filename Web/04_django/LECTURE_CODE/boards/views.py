b85aca034f793f61d84bfac2862f97e3114a2859
athletejuan <vanillasky84.0627@gmail.com>
2019-09-22 18:54:35 +0900

    Django GET/POST merge

--- Web/04_django/LECTURE_CODE/boards/views.py
+++ Web/04_django/LECTURE_CODE/boards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
 
 def index(request):
@@ -7,15 +7,18 @@ def index(request):
         'articles':articles,
     })
 
-def new(request):
-    return render(request, 'new.html')
+# def new(request):
+#     return render(request, 'new.html')
 
 def create(request):
-    article = Article()
-    article.title = request.GET.get('title')
-    article.content = request.GET.get('content')
-    article.save()
-    return render(request, 'create.html')
+    if request.method == "POST":
+        article = Article()
+        article.title = request.POST.get('title')
+        article.content = request.POST.get('content')
+        article.save()
+        return redirect('/boards/')
+    else:
+        return render(request, 'create.html')
     
     # 2nd method
     # title = request.GET.get('title')
@@ -32,23 +35,23 @@ def detail(request, article_id):
         'article':article,
     })
 
-def edit(request, article_id):
-    article = Article.objects.get(id=article_id)
-    return render(request, 'edit.html', {
-        'article':article,
-    })
+# def edit(request, article_id):
+#     article = Article.objects.get(id=article_id)
+#     return render(request, 'edit.html', {
+#         'article':article,
+#     })
 
 def update(request, article_id):
     article = Article.objects.get(id=article_id)
-    article.title = request.GET.get('title')
-    article.content = request.GET.get('content')
-    article.save()
-    return render(request, 'update.html')
+    if request.method == "POST":
+        article.title = request.POST.get('title')
+        article.content = request.POST.get('content')
+        article.save()
+        return redirect(f'/boards/{article.id}/')
+    else:
+        return render(request, 'update.html', {'article':article})
 
 def delete(request, article_id):
     article = Article.objects.get(id=article_id)
     article.delete()
-    articles = Article.objects.all()[::-1]
-    return render(request, 'index.html', {
-        'articles':articles
-    })
+    return redirect('/boards/')
