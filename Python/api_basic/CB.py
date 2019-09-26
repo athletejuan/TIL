159a8321eb97984f7a386da08dffb984b5ef76b4
athletejuan <vanillasky84.0627@gmail.com>
2019-09-27 07:04:44 +0900

    Python Basic API Ex

---
+++ Python/api_basic/CB.py
@@ -0,0 +1,35 @@
+import requests
+from decouple import config
+from pprint import pprint as pp
+
+# url = 'https://api.telegram.org/bot<token>/METHOD_NAME'
+
+base = 'https://api.telegram.org'
+token = config('TOKEN')
+
+url = f'{base}/{token}/getUpdates'
+
+res = requests.get(url)
+data = res.json()
+# pp(data)
+
+# 처음 메시지 보낸 사람에게 응답하기
+chat_id = data['result'][0]['message']['chat']['id']
+text = "2019.10.18 독립예정일"
+
+url = f'{base}/{token}/sendMessage?chat_id={chat_id}&text={text}'
+
+# requests.get(url)
+# data = res.json()
+# print(data)
+
+# 메시지 보낸 모든 사람에게 응답하기
+senders = []
+for i in data['result']:
+    senders.append(i['message']['chat']['id'])
+
+receivers = set(senders)
+
+for i in receivers:
+    url = f'{base}/{token}/sendMessage?chat_id={i}&text={text}'
+    requests.get(url)
