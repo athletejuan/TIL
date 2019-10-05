9a189fd7decbb2c66848b57d19e4780275b55338
athletejuan <vanillasky84.0627@gmail.com>
2019-10-06 00:30:47 +0900

    879th Lotto again Ex

--- Python/api_basic/lotto_api.py
+++ Python/api_basic/lotto_api.py
@@ -1,12 +1,9 @@
 import requests
+import random
 
-url = 'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=874'
+url = 'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=879'
 
 res = requests.get(url)
-
-# print(res.text)
-# print(res.json())
-
 data = res.json()
 
 winner = []
@@ -14,4 +11,24 @@ winner = []
 for i in range(1,7):
     winner.append(data[f'drwtNo{i}'])
 
-print(winner)
+lotto = sorted(random.sample(range(1,46),6))
+match = set(lotto)&set(winner)
+rank = len(match)
+cnt = 0
+
+while rank < 5:
+    lotto = sorted(random.sample(range(1,46),6))
+    match = set(lotto)&set(winner)
+    rank = len(match)
+    cnt += 1
+    if rank == 6:
+        print("OMG! 1st price!!")
+    elif rank == 5:
+        print("Awesome! 3rd price!")
+    elif rank == 4:
+        print("Very Good! 4th grade")
+    elif rank == 3:
+        print(f"Winner number is {winner}, My number is {lotto}, {match}, Not bad. 5th grade")
+    else:
+        print(f"Winner number is {winner}, My number is {lotto}, {match}, WTF!!")
+print(cnt)
