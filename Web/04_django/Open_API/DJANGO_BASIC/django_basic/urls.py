f400c084340c8d3651d794e55350513dfb3868df
athletejuan <vanillasky84.0627@gmail.com>
2019-10-29 00:31:06 +0900

    Django_variable_routing

--- Web/04_django/Open_API/DJANGO_BASIC/django_basic/urls.py
+++ Web/04_django/Open_API/DJANGO_BASIC/django_basic/urls.py
@@ -4,12 +4,19 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
+    path('home/static_example/', views.static_example),
+    path('home/user_create/', views.user_create),
+    path('home/user_new/', views.user_new),
     path('home/palin/', views.palin),
     path('home/word/', views.word),
+    path('home/catch/', views.catch),
+    path('home/throw/', views.throw),
     path('home/ispal/<word>/', views.ispal),
+    path('home/isbirth/', views.isbirth),
     path('home/image/', views.image),
     path('home/template_language/', views.template_language),
     path('home/square/<int:weight>/<int:height>/', views.square),
+    path('home/introduce/<name>/<int:age>/', views.introduce),
     path('home/hello/<name>/', views.hello),
     path('home/lotto/', views.lotto),
     path('home/dinner/', views.dinner),
--- Web/04_django/Open_API/DJANGO_BASIC/home/views.py
+++ Web/04_django/Open_API/DJANGO_BASIC/home/views.py
@@ -21,7 +21,7 @@ def dinner(request):
 # import requests
 def lotto(request):
     numbers = range(1,46)
-    lotto_nums = random.sample(numbers, 6)
+    my_lotto = random.sample(numbers, 6)
     # last_lotto = [18, 34, 39, 43, 44, 45]
 
     url = 'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=882'
@@ -34,20 +34,25 @@ def lotto(request):
     for i in range(1,7):
         winner.append(data[f'drwtNo{i}'])
 
-    match = set(lotto_nums) & set(winner)
+    match = set(my_lotto) & set(winner)
     rank = len(match)
     cnt = 0
 
     while rank < 5:
-        lotto_nums = random.sample(range(1,46),6)
-        match = set(lotto_nums) & set(winner)
+        my_lotto = random.sample(range(1,46),6)
+        match = set(my_lotto) & set(winner)
         rank = len(match)
         cnt += 1
     # return HttpResponse(f'오늘의 로또 추천 번호는 {lotto_nums}입니다.')
-    return render(request, 'lotto.html', {'lotto_nums':lotto_nums, 'winner':winner, 'cnt':cnt})
+    return render(request, 'lotto.html', {'my_lotto':my_lotto, 'winner':winner, 'cnt':cnt})
+
+# Variable Routing
 
 def hello(request, name):
     return render(request, 'hello.html', {'name':name})
+
+def introduce(request, name, age):
+    return render(request, 'introduce.html', {'name':name, 'age':age})
 
 def square(request, weight, height):
     sq = weight * height
@@ -58,13 +63,13 @@ def square(request, weight, height):
 def template_language(request):
     menus = ['아메리카노', '카페라떼', '마키아또', '루이보스', '프라푸치노']
     my_sentence = 'Life is short, you need python'
-    cafe = ['starbucks', 'coffeebean', 'hollys', 'ediya']
+    cafes = ['starbucks', 'coffeebean', 'hollys', 'ediya']
     datetimenow = datetime.now()    # from datetime import datetime 
     empty_list = []
     context = {
         'menus': menus,
         'my_sentence': my_sentence,
-        'cafe': cafe,
+        'cafes': cafes,
         'empty_list': empty_list,
         'datetimenow': datetimenow,
     }
@@ -73,21 +78,63 @@ def template_language(request):
 def image(request):
     return render(request, 'image.html')
 
+def isbirth(request):
+    # 6월 27일이면 예, 아니면 아니오
+    today = datetime.now()
+    if today.month == 6 and today.day == 27:
+        result = True
+    else:
+        result = False
+    # context = {'result': result}
+    return render(request, 'isbirth.html', {'result': result})
+
 def ispal(request, word):
     if word == word[::-1]:
         result = True
     else:
         result = False
-    context = {'word': word, 'result': result}
-    return render(request, 'ispal.html', context)
+    # context = {'word': word, 'result': result}
+    return render(request, 'ispal.html', {'word': word, 'result': result})
+
+# GET / POST
+
+def throw(request):
+    return render(request, 'throw.html')
+
+def catch(request):
+    message = request.GET.get('message')
+    # message2 = request.GET.get('message2')
+    context = {
+        'message':message,
+        # 'message2':message2,
+    }
+    return render(request, 'catch.html', context)
 
 def word(request):
     return render(request, 'word.html')
 
 def palin(request):
-    ispal2 = request.GET.get('input_word')
+    # ispal2 = request.GET.get('input_word')
+    ispal2 = request.GET['input_word']
     if ispal2 == ispal2[::-1]:
         result = True
     else:
         result = False
     return render(request, 'palin.html', {'ispal2':ispal2, 'result':result})
+
+# POST
+
+def user_new(request):
+    return render(request, 'user_new.html')
+
+def user_create(request):
+    user_name = request.GET.get('name')
+    user_password = request.GET.get('pwd')
+    # user_name = request.POST.get('name')
+    # user_password = request.POST.get('pwd')
+    return render(request, 'user_create.html', {'user_name':user_name, 'user_password':user_password})
+
+# Static File
+
+def static_example(request):
+    return render(request, 'static_example.html')
