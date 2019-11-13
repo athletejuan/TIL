16d86607b54b05b59bc68e01c9c7d5ed7cdbd9ee
athletejuan <vanillasky84.0627@gmail.com>
2019-11-14 00:38:03 +0900

    Django ModelRelation

---
+++ Web/04_django/Open_API/MODELRELATION/onetomany/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class User(models.Model):
+    name = models.TextField()
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Article(models.Model):
+    title = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title}'
+        
+class Comment(models.Model):
+    content = models.TextField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.content}'
