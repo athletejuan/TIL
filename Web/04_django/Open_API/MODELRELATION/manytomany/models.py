0af10ac8b96b7f08a7359d29e16457056647016a
athletejuan <vanillasky84.0627@gmail.com>
2019-11-14 16:21:12 +0900

    Django ModelRelation M:N

--- Web/04_django/Open_API/MODELRELATION/manytomany/models.py
+++ Web/04_django/Open_API/MODELRELATION/manytomany/models.py
@@ -8,14 +8,16 @@ class Doctor(models.Model):
 
 class Patient(models.Model):
     name = models.CharField(max_length=20)
-    doctors = models.ManyToManyField(Doctor, through='Reservation', related_name='patients')
+    # doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    # doctors = models.ManyToManyField(Doctor, through='Reservation', related_name='patients')
+    doctors = models.ManyToManyField(Doctor, related_name='patients')
 
     def __str__(self):
         return f'{self.id}번 환자 {self.name}'
 
-class Reservation(models.Model):
-    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
-    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+# class Reservation(models.Model):
+#     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+#     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return f'{self.doctor_id}번 의사의 {self.patient_id}번 환자'
+#     def __str__(self):
+#         return f'{self.doctor_id}번 의사의 {self.patient_id}번 환자'no
