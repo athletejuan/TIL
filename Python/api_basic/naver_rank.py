d47b7f5dff823d4db1faf91a54ff10ce35b8702e
athletejuan <vanillasky84.0627@gmail.com>
2019-12-10 17:11:16 +0900

    Naver_Rank

---
+++ Python/api_basic/naver_rank.py
@@ -0,0 +1,17 @@
+import requests
+import bs4 
+import datetime
+
+html = requests.get('https://www.naver.com/').text
+soup = bs4.BeautifulSoup(html, 'html.parser')
+# firefox에서는 css path를 복사해야 함.
+tags = soup.select('.PM_CL_realtimeKeyword_rolling .ah_item .ah_k')
+# print(tags)
+
+now = datetime.datetime.now()
+print(f'{now} 기준 네이버 검색어 순위')
+# with open('naver.txt', 'w') as f:
+#     f.write(f'{now} 기준 네이버 검색어 순위\r\n')
+for i, tag in enumerate(tags):
+    print(f'{i+1}. {tag.text}')
+    # f.write(f'{i+1}. {tag.text}\r\n')
