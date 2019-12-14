2e2a6723e71bcedde0a73a7c774b576d8cd3e6df
athletejuan <vanillasky84.0627@gmail.com>
2019-12-14 22:38:27 +0900

    Flask_Chatbot_CFR_init

--- Python/FLASK_EX/FLASK_CHATBOT/app.py
+++ Python/FLASK_EX/FLASK_CHATBOT/app.py
@@ -36,34 +36,39 @@ def telegram():
     if from_telegram.get('message') is not None:    # NoneType일 경우 예외처리
         chat_id = from_telegram.get('message').get('from').get('id')
         text = from_telegram.get('message').get('text')
+        # CFR
+        if from_telegram.get('message').get('photo') is not None:
+
+
         # 파파고 번역
-        if text[0:4] == '/한영 ':
+        else:
+            if text[0:4] == '/한영 ':
             headers = {'X-Naver-Client-Id': naver_client_id, 'X-Naver-Client-Secret': naver_client_secret}
             data = {'source':'ko', 'target':'en', 'text':text[4:]}
             papago_res = requests.post('https://openapi.naver.com/v1/papago/n2mt', headers=headers, data=data)
             text = papago_res.json().get('message').get('result').get('translatedText')
-        elif text[0:4] == '/영한 ':
-            headers = {'X-Naver-Client-Id': naver_client_id, 'X-Naver-Client-Secret': naver_client_secret}
-            data = {'source':'en', 'target':'ko', 'text':text[4:]}
-            papago_res = requests.post('https://openapi.naver.com/v1/papago/n2mt', headers=headers, data=data)
-            text = papago_res.json().get('message').get('result').get('translatedText')
-        elif text[0:4] == '/한불 ':
-            headers = {'X-Naver-Client-Id': naver_client_id, 'X-Naver-Client-Secret': naver_client_secret}
-            data = {'source':'ko', 'target':'fr', 'text':text[4:]}
-            papago_res = requests.post('https://openapi.naver.com/v1/papago/n2mt', headers=headers, data=data)
-            text = papago_res.json().get('message').get('result').get('translatedText')
-        # 로또
-        elif text == '로또추천':
-            text = sorted(random.sample(range(1,46),6))
-        elif text[:4] == '로또확인':
-            num = text[4:]
-            url = f'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={num}'
-            res = requests.get(url).json()
+            elif text[0:4] == '/영한 ':
+                headers = {'X-Naver-Client-Id': naver_client_id, 'X-Naver-Client-Secret': naver_client_secret}
+                data = {'source':'en', 'target':'ko', 'text':text[4:]}
+                papago_res = requests.post('https://openapi.naver.com/v1/papago/n2mt', headers=headers, data=data)
+                text = papago_res.json().get('message').get('result').get('translatedText')
+            elif text[0:4] == '/한불 ':
+                headers = {'X-Naver-Client-Id': naver_client_id, 'X-Naver-Client-Secret': naver_client_secret}
+                data = {'source':'ko', 'target':'fr', 'text':text[4:]}
+                papago_res = requests.post('https://openapi.naver.com/v1/papago/n2mt', headers=headers, data=data)
+                text = papago_res.json().get('message').get('result').get('translatedText')
+            # 로또
+            elif text == '로또추천':
+                text = sorted(random.sample(range(1,46),6))
+            elif text[:4] == '로또확인':
+                num = text[4:]
+                url = f'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={num}'
+                res = requests.get(url).json()
 
-            winner = []
-            for i in range(1,7):
-                winner.append(res[f'drwtNo{i}'])
-            bonus_num = res['bnusNo']
-            text = f'로또 {num}회차의 당첨번호는 {winner}이고, 보너스번호는 {bonus_num}입니다.'
+                winner = []
+                for i in range(1,7):
+                    winner.append(res[f'drwtNo{i}'])
+                bonus_num = res['bnusNo']
+                text = f'로또 {num}회차의 당첨번호는 {winner}이고, 보너스번호는 {bonus_num}입니다.'
         requests.get(f'{base}/bot{token}/sendMessage?chat_id={chat_id}&text={text}')
     return '', 200
