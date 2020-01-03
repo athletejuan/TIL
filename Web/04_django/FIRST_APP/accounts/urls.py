8f08a61f040f2d5445ced7f8ed6e85d896dad436
athletejuan <vanillasky84.0627@gmail.com>
2020-01-03 19:34:06 +0900

    Django_accounts_url

--- Web/04_django/FIRST_APP/accounts/urls.py
+++ Web/04_django/FIRST_APP/accounts/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login', views.login, name='login'),
-    path('signup', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
 ]
