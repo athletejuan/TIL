f060a49323bebc6d023456cb23b58d0056c5fe57
athletejuan <vanillasky84.0627@gmail.com>
2020-01-03 19:30:29 +0900

    Django_Authentication

--- Web/04_django/PRACTICE/practice/urls.py
+++ Web/04_django/PRACTICE/practice/urls.py
@@ -6,4 +6,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('board/', include('board.urls')),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
