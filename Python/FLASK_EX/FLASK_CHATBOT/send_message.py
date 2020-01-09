01ddf6396bfdc7e49772ba4d3aafd4865228183c
athletejuan <vanillasky84.0627@gmail.com>
2020-01-09 22:00:23 +0900

    Flask_Chatbot_for_SS3rd

--- Python/FLASK_EX/FLASK_CHATBOT/send_message.py
+++ Python/FLASK_EX/FLASK_CHATBOT/send_message.py
@@ -1,11 +1,19 @@
 import requests
 from decouple import config
+from pprint import pprint as pp
 
-base = 'https://api.telegram.org'
+# 기본 설정
 token = config('TOKEN')
-chat_id = config('CHAT_ID')
-text = 'pythonanywhere deployment'
+app_url = f'https://api.telegram.org/bot{token}'
 
-send_message = requests.get(f'{base}/bot{token}/sendMessage?chat_id={chat_id}&text={text}')
+# 응답 내용 저장하기
+update_url = f'{app_url}/getUpdates'
+response = requests.get(update_url).json()
 
-print(send_message.text)
+# chat_id 찾아서 꺼내기
+chat_id = response.get('result')[0].get('message').get('chat').get('id')
+text = '안녕하세요'
+
+message_url = f'{app_url}/sendMessage?chat_id={chat_id}&text={text}'
+
+print(requests.get(message_url))
