5c609526b63ca33bcaddbaec2ec93c97ff66852c
athletejuan <vanillasky84.0627@gmail.com>
2020-01-10 16:51:41 +0900

    Flask_Chatbot_for_SS3rd

--- Python/FLASK_EX/FLASK_CHATBOT/app.py
+++ Python/FLASK_EX/FLASK_CHATBOT/app.py
@@ -22,5 +22,17 @@ def send():
     requests.get(message_url)
     return render_template('send.html')
 
+@app.route('/telegram', methods=['POST'])
+def telegram():
+    # step 1. 구조 print해보기 & 변수 저장
+    telegram_response = request.get_json()
+
+    # step 2. 그대로 돌려보내기(echo)
+    if telegram_response.get('message') is not None:
+        chat_id = telegram_response.get('message').get('from').get('id')
+        text = telegram_response.get('message').get('text')
+        requests.get(f'{app_url}/sendMessage?chat_id={chat_id}&text={text}')
+    return '', 200
+
 if __name__=='__main__':
     app.run(debug=True)
---
+++ Python/FLASK_EX/FLASK_CHATBOT/set_webhook.py
@@ -0,0 +1,10 @@
+from decouple import config
+import requests
+
+token = config('TOKEN')
+app_url = f'https://api.telegram.org/bot{token}'
+ngrok_url = 'https://9191845c.ngrok.io'
+set_webhook_url = f'{app_url}/setWebhook?url={ngrok_url}/telegram'
+
+response = requests.get(set_webhook_url)
+print(response.text)
