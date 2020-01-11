f67b0b955359f0398c307b387f3c6bfe23f3f974
athletejuan <vanillasky84.0627@gmail.com>
2020-01-11 12:35:05 +0900

    Flask_Chatbot_for_SS3rd

--- Python/FLASK_EX/FLASK_CHATBOT/app.py
+++ Python/FLASK_EX/FLASK_CHATBOT/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request
 import requests
 from decouple import config
+from pprint import pprint as pp
 app = Flask(__name__)
 
 token = config('TOKEN')
 chat_id = config('CHAT_ID')
+naver_id = config('NAVER_CLIENT_ID')
+naver_secret = config('NAVER_CLIENT_SECRET')
 app_url = f'https://api.telegram.org/bot{token}'
 
 @app.route('/')
@@ -31,6 +34,56 @@ def telegram():
     if telegram_response.get('message') is not None:
         chat_id = telegram_response.get('message').get('from').get('id')
         text = telegram_response.get('message').get('text')
+        if text[0:4] == '/한영 ':
+            headers = {
+                'X-Naver-Client-Id': naver_id,
+                'X-Naver-Client-Secret': naver_secret,
+            }
+            data = {'source':'ko', 'target':'en', 'text':text[4:]}
+            papago_response = requests.post(
+                'https://openapi.naver.com/v1/papago/n2mt', headers=headers, data=data
+            )
+            text = papago_response.json().get('message').get('result').get('translatedText')
+        elif text[0:4] == '/영한 ':
+            headers = {
+                'X-Naver-Client-Id': naver_id,
+                'X-Naver-Client-Secret': naver_secret,
+            }
+            data = {'source':'en', 'target':'ko', 'text':text[4:]}
+            papago_response = requests.post(
+                'https://openapi.naver.com/v1/papago/n2mt', headers=headers, data=data
+            )
+            text = papago_response.json().get('message').get('result').get('translatedText')
+        elif text[0:4] == '/한불 ':
+            headers = {
+                'X-Naver-Client-Id': naver_id,
+                'X-Naver-Client-Secret': naver_secret,
+            }
+            data = {'source':'ko', 'target':'fr', 'text':text[4:]}
+            papago_response = requests.post(
+                'https://openapi.naver.com/v1/papago/n2mt', headers=headers, data=data
+            )
+            text = papago_response.json().get('message').get('result').get('translatedText')
+        elif text[0:4] == '/한중 ':
+            headers = {
+                'X-Naver-Client-Id': naver_id,
+                'X-Naver-Client-Secret': naver_secret,
+            }
+            data = {'source':'ko', 'target':'zh-CN', 'text':text[4:]}
+            papago_response = requests.post(
+                'https://openapi.naver.com/v1/papago/n2mt', headers=headers, data=data
+            )
+        elif text[0:4] == '/월남 ':
+            headers = {
+                'X-Naver-Client-Id': naver_id,
+                'X-Naver-Client-Secret': naver_secret,
+            }
+            data = {'source':'ko', 'target':'vi', 'text':text[4:]}
+            papago_response = requests.post(
+                'https://openapi.naver.com/v1/papago/n2mt', headers=headers, data=data
+            )
+            pp(papago_response.json())
+            text = papago_response.json().get('message').get('result').get('translatedText')
         requests.get(f'{app_url}/sendMessage?chat_id={chat_id}&text={text}')
     return '', 200
 
--- Python/FLASK_EX/FLASK_CHATBOT/set_webhook.py
+++ Python/FLASK_EX/FLASK_CHATBOT/set_webhook.py
@@ -3,7 +3,7 @@ import requests
 
 token = config('TOKEN')
 app_url = f'https://api.telegram.org/bot{token}'
-ngrok_url = 'https://9191845c.ngrok.io'
+ngrok_url = 'https://be7637c1.ngrok.io'
 set_webhook_url = f'{app_url}/setWebhook?url={ngrok_url}/telegram'
 
 response = requests.get(set_webhook_url)
