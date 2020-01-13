14c0aa71e63c1a3999540368ea0f1baa832ad62e
athletejuan <vanillasky84.0627@gmail.com>
2020-01-13 09:45:16 +0900

    Flask_Chatbot_set_webhook_for_pythonnaywhere

--- Python/FLASK_EX/FLASK_CHATBOT/set_webhook.py
+++ Python/FLASK_EX/FLASK_CHATBOT/set_webhook.py
@@ -3,8 +3,10 @@ import requests
 
 token = config('TOKEN')
 app_url = f'https://api.telegram.org/bot{token}'
-ngrok_url = 'https://be7637c1.ngrok.io'
-set_webhook_url = f'{app_url}/setWebhook?url={ngrok_url}/telegram'
+pythonanywhere_url = 'https://###.pythonanywhere.com'
+ngrok_url = '###'
+# set_webhook_url = f'{app_url}/setWebhook?url={ngrok_url}/telegram'
+set_webhook_url = f'{app_url}/setWebhook?url={pythonanywhere_url}/telegram'    # pythonanywhere 배포시
 
 response = requests.get(set_webhook_url)
 print(response.text)
