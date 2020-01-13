19a24f82092751c84a7ab3c8ada51a211c1a6bf8
athletejuan <vanillasky84.0627@gmail.com>
2020-01-14 01:55:34 +0900

    Bithumb_API_for_SS3rd

--- Python/api_basic/bitcoin.py
+++ Python/api_basic/bitcoin.py
@@ -3,14 +3,17 @@ from pprint import pprint as pp
 
 url = 'https://api.bithumb.com/public/ticker/all'
 bithumb = requests.get(url).json()['data']
+pp(bithumb)
 
 for key,value in bithumb.items():
     if type(value) == type(dict()):
         now = float(value['closing_price'])
-        start = float(value['opening_price'])
-        if now - start > 0:
+        prev_closing = float(value['prev_closing_price'])
+        if now - prev_closing > 0:
             print(f'{key}: 상승장')
-        else:
+        elif now - prev_closing < 0:
             print(f'{key}: 하락장')
+        else:
+            print(f'{key}: 변동없음')
     else:
         continue
