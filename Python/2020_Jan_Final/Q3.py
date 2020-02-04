ab63b8866dca6d106b4101c4a44c820332c23ed7
athletejuan <vanillasky84.0627@gmail.com>
2020-02-04 18:37:56 +0900

    SS3rd_Jan_Final_#3_edit

--- Python/2020_Jan_Final/Q3.py
+++ Python/2020_Jan_Final/Q3.py
@@ -3,9 +3,16 @@ def q2(num):
         if (num % 100) % 3 == 0:
             if num % 4 == 0:
                 return True
-    else:
-        return False
+    return False
         
 print(q2(512))
 print(q2(384))
-print(q2(321))
+print(q2(321))  # 3의 배수기만 함
+print(q2(100))
+print(q2(311))
+print(q2(520))  # 3의 배수가 아님
+print(q2(102))  # 2의 배수 + 전체가 3의 배수 + 4의 배수가 아님
+print(q2(120))  # 2의 배수 + 전체가 3의 배수 + 4의 배수
+print(q2(354))  # 2의 배수 + 3의 배수 + 4의 배수가 아님
+print(q2(460))  # 다맞는데 마지막 0
+print(q2(400))  # 다맞는데 0, 00
