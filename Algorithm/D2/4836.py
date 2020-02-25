23129f1fa5c2af68bd44c9d579a0e9281830b84e
athletejuan <vanillasky84.0627@gmail.com>
2020-02-26 00:17:20 +0900

    SWEA_PS_LIST2_cross_painting

---
+++ Algorithm/D2/4836.py
@@ -0,0 +1,22 @@
+T = int(input())
+
+for test_case in range(1, T+1):
+    num = int(input())
+    red = []
+    blue = []
+    count = 0
+    for i in range(num):
+        reg = list(map(int, input().split()))
+        if reg[4]%2:
+            for x in range(reg[0], reg[2]+1):
+                for y in range(reg[1], reg[3]+1):
+                    red.append((x,y))
+        else:
+            for x in range(reg[0], reg[2]+1):
+                for y in range(reg[1], reg[3]+1):
+                    blue.append((x,y))
+    for j in red:
+        if j in blue:
+            count += 1
+    print(f'#{test_case} {count}')
+            
