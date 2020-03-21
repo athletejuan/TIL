9e7cb174c9805953bbd6099e1031a38d279efbd6
athletejuan <vanillasky84.0627@gmail.com>
2020-03-21 21:23:02 +0900

    Django_CRUD_basic

---
+++ Web/04_django/Corona/CORONA02/articles/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('new/', views.new),
+    path('create/', views.create),
+    path('<int:article_id>/', views.detail),
+    path('<int:article_id>/edit/', views.edit),
+    path('<int:article_id>/update/', views.update),
+    path('<int:article_id>/delete/', views.delete),
+]
