1712c74ff0a0c1326924fbced1275a3c80512516
athletejuan <vanillasky84.0627@gmail.com>
2020-03-23 00:39:13 +0900

    Django_model_class_Comment

--- Web/04_django/Corona/CORONA02/articles/models.py
+++ Web/04_django/Corona/CORONA02/articles/models.py
@@ -8,3 +8,12 @@ class Article(models.Model):
 
     def __str__(self):
         return f'{self.id}번 글 - {self.title}'
+
+class Comment(models.Model):
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'<article({self.articld_id}) : Comment({self.id})> - {self.content}'
