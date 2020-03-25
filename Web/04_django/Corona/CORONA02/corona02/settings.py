87afd26ee8a834a5f64531cab3c95d2e0d020af3
athletejuan <vanillasky84.0627@gmail.com>
2020-03-26 01:56:06 +0900

    Django_Image_basic

--- Web/04_django/Corona/CORONA02/corona02/settings.py
+++ Web/04_django/Corona/CORONA02/corona02/settings.py
@@ -109,3 +109,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'corona02','assets','images'),
+]
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
