ca1debe009199d160821385697bb2242e451cdc2
athletejuan <vanillasky84.0627@gmail.com>
2020-03-29 02:48:14 +0900

    Django_accounts_part01

--- Web/04_django/Corona/CORONA02/corona02/settings.py
+++ Web/04_django/Corona/CORONA02/corona02/settings.py
@@ -20,6 +20,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'articles',
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Web/04_django/Corona/CORONA02/corona02/urls.py
+++ Web/04_django/Corona/CORONA02/corona02/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('accounts/', include('accounts.urls')),
     path('articles/', include('articles.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
