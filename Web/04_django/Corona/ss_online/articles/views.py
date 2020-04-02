62d17aa2f760d479ccb4131f78884b6d888a83b8
athletejuan <vanillasky84.0627@gmail.com>
2020-04-02 20:03:22 +0900

    Django_ss_online_PJT01

--- Web/04_django/Corona/ss_online/articles/views.py
+++ Web/04_django/Corona/ss_online/articles/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Article
 
-# Create your views here.
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        'articles':articles
+    }
+    return render(request, 'articles/index.html', context)
+
+def new(request):
+    return render(request, 'articles/new.html')
+
+def create(request):
+    article = Article()
+    article.title = request.GET.get('title')
+    article.content = request.GET.get('content')
+    article.save()
+    return render(request, 'articles/create.html')
