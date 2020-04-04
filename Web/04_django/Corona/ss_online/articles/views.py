c45c32e465e86d70b1a6e963ef85b97b9e7d4e17
athletejuan <vanillasky84.0627@gmail.com>
2020-04-05 01:32:39 +0900

    Django_ss_online_02

--- Web/04_django/Corona/ss_online/articles/views.py
+++ Web/04_django/Corona/ss_online/articles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
 
 def index(request):
@@ -17,3 +17,29 @@ def create(request):
     article.content = request.GET.get('content')
     article.save()
     return render(request, 'articles/create.html')
+
+def detail(request, article_id):
+    article = Article.objects.get(id=article_id)
+    context = {
+        'article':article
+    }
+    return render(request, 'articles/detail.html', context)
+
+def edit(request, article_id):
+    article = Article.objects.get(id=article_id)
+    context = {
+        'article':article
+    }
+    return render(request, 'articles/edit.html', context)
+
+def update(request, article_id):
+    article = Article.objects.get(id=article_id)
+    article.title = request.GET.get('title')
+    article.content = request.GET.get('content')
+    article.save()
+    return redirect(f'/articles/{ article.id }')
+
+def delete(request, article_id):
+    article = Article.objects.get(id=article_id)
+    article.delete()
+    return redirect('/articles/')
--- Web/04_django/Corona/ss_online/ss_online/settings.py
+++ Web/04_django/Corona/ss_online/ss_online/settings.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'ss_online.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'ss_online','templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
