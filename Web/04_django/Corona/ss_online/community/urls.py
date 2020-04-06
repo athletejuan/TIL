7207cb7c5d53e0e702abde4b8bab72cf7c2b18c8
athletejuan <vanillasky84.0627@gmail.com>
2020-04-07 00:40:44 +0900

    Django_ss_online_PJT02

--- Web/04_django/Corona/ss_online/community/urls.py
+++ Web/04_django/Corona/ss_online/community/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = "community"
+
 urlpatterns = [
-    path('', views.review_list),
-    path('new_review/', views.new_review),
-    path('review_create/', views.review_create),
-    path('<int:review_id>/', views.review_detail),
+    path('', views.review_list, name="review_list"),
+    path('new_review/', views.new_review, name="new_review"),
+    # path('review_create/', views.review_create, name="review_create"),
+    path('<int:review_id>/', views.review_detail, name="review_detail"),
 ]
--- Web/04_django/Corona/ss_online/community/views.py
+++ Web/04_django/Corona/ss_online/community/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Review
+from .forms import ReviewForm
 
 def review_list(request):
     reviews = Review.objects.all()
@@ -9,18 +10,29 @@ def review_list(request):
     return render(request, 'reviews/review_list.html', context)
 
 def new_review(request):
-    return render(request, 'reviews/new_review.html')
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save()
+            return redirect('community:review_detail', review.pk)
+    else:
+        form = ReviewForm()
+        context = {
+            'form':form
+        }
+        return render(request, 'reviews/new_review.html', context)
 
-def review_create(request):
-    review = Review()
-    review.title = request.GET.get('title')
-    review.content = request.GET.get('content')
-    review.rank = request.GET.get('rank')
-    review.save()
-    return redirect('/community/')
+# def review_create(request):
+#     review = Review()
+#     review.title = request.GET.get('title')
+#     review.content = request.GET.get('content')
+#     review.rank = request.GET.get('rank')
+#     review.save()
+#     return redirect('/community/')
 
 def review_detail(request, review_id):
-    review = Review.objects.get(id=review_id)
+    # review = Review.objects.get(id=review_id)
+    review = get_object_or_404(Review, id=review_id)
     context = {
         'review':review
     }
