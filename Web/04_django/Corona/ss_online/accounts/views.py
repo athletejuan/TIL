1bd8b25ef40dce9c5414d1f8f8fdd3cebfa8dc9a
athletejuan <vanillasky84.0627@gmail.com>
2020-04-12 21:12:56 +0900

    Django_ss_online_05_workshop

--- Web/04_django/Corona/ss_online/accounts/views.py
+++ Web/04_django/Corona/ss_online/accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import get_user_model
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 User = get_user_model()
 
@@ -16,6 +18,7 @@ def signup(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
+            auth_login(request, user)
             return redirect('articles:index')
     else:
         form = UserCreationForm()
@@ -24,3 +27,20 @@ def signup(request):
     }
     return render(request, 'accounts/signup.html', context)
 
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('articles:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form':form
+    }
+    return render(request, 'accounts/login.html', context)
+
+def logout(request):
+    # if request.method == "POST":
+    auth_logout(request)
+    return redirect('articles:index')
