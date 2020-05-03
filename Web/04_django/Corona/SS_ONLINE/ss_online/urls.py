5e081afd9ab96fd478d9c4d45fd7fdc778699b90
athletejuan <vanillasky84.0627@gmail.com>
2020-05-03 18:29:36 +0900

    Django_ss_online_08_homework

--- Web/04_django/Corona/SS_ONLINE/ss_online/urls.py
+++ Web/04_django/Corona/SS_ONLINE/ss_online/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from articles import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +10,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('community/', include('community.urls')),
     path('eithers/', include('eithers.urls')),
+    path('', views.index),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
