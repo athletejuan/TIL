8d8b34c408bf78e035ae651a06ecc27a7584b082
athletejuan <vanillasky84.0627@gmail.com>
2020-05-20 15:41:31 +0900

    Vue_component_with_drf_part02

--- Vue/vue_study/03_vue_component_with_drf/todo-back/todoback/urls.py
+++ Vue/vue_study/03_vue_component_with_drf/todo-back/todoback/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-token-auth/', obtain_jwt_token),
+    path('api/v1/', include('todos.urls')),
 ]
--- Vue/vue_study/03_vue_component_with_drf/todo-back/todos/views.py
+++ Vue/vue_study/03_vue_component_with_drf/todo-back/todos/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse, JsonResponse
+from .models import Todo
+from .serializers import TodoSerializer
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
-# Create your views here.
+@api_view(['POST'])
+@permission_classes((IsAuthenticated,))
+@authentication_classes((JSONWebTokenAuthentication,))
+def todo_create(request):
+    serializer = TodoSerializer(data=request.POST)
+    if serializer.is_valid():
+        serializer.save()
+        return JsonResponse(serializer.data)
+    return HttpResponse(status=400)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes((IsAuthenticated,))
+@authentication_classes((JSONWebTokenAuthentication,))
+def todo_detail(request, id):
+    todo = get_object_or_404(Todo, id=id)
+    if request.method == 'GET':
+        serializer = TodoSerializer(todo)
+        return JsonResponse(serializer.data)
+    elif request.method == 'PUT':
+        serializer = TodoSerializer(todo, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    elif request.method == 'DELETE':
+        todo.delete()
+        return HttpResponse(status=204)
