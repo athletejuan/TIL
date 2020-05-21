6f946fc5e617137c7dca7dceed7a9e2b83bdd443
athletejuan <vanillasky84.0627@gmail.com>
2020-05-21 17:16:21 +0900

    Vue_component_with_drf_user_CR

--- Vue/vue_study/03_vue_component_with_drf/todo-back/todos/serializers.py
+++ Vue/vue_study/03_vue_component_with_drf/todo-back/todos/serializers.py
@@ -1,7 +1,21 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import Todo
+
+User = get_user_model()
 
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = ['id', 'user', 'title', 'completed', ]
+
+class UserCreationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password', ]
+
+class UserSerializer(serializers.ModelSerializer):
+    todo_set = TodoSerializer(many=True)
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'todo_set', ]
--- Vue/vue_study/03_vue_component_with_drf/todo-back/todos/views.py
+++ Vue/vue_study/03_vue_component_with_drf/todo-back/todos/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
 from django.http import HttpResponse, JsonResponse
 from .models import Todo
-from .serializers import TodoSerializer
+from .serializers import TodoSerializer, UserCreationSerializer, UserSerializer
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+
+User = get_user_model()
 
 @api_view(['POST'])
 @permission_classes((IsAuthenticated,))
@@ -33,3 +36,25 @@ def todo_detail(request, id):
     elif request.method == 'DELETE':
         todo.delete()
         return HttpResponse(status=204)
+
+@api_view(['POST'])
+@permission_classes((AllowAny, ))
+def user_signup(request):
+    serializer = UserCreationSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        user = serializer.save()
+        user.set_password(request.data.get('password'))
+        user.save()
+        print(serializer.data)
+        return JsonResponse({'message': '회원가입이 성공적으로 완료되었습니다.'})
+
+@api_view(['GET'])
+@permission_classes((IsAuthenticated, ))
+@authentication_classes((JSONWebTokenAuthentication, ))
+def user_detail(request, id):
+    user = get_object_or_404(User, pk=id)
+    if request.user != user:
+        return HttpResponse(status=403)
+    elif request.method == 'GET':
+        serializer = UserSerializer(user)
+        return JsonResponse(serializer.data)
