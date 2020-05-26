8c3f95425eb834a19f2b2a306d4359d583a5cc80
athletejuan <vanillasky84.0627@gmail.com>
2020-05-27 02:28:32 +0900

    Vue_Day03_vue-cli

---
+++ Vue/vue_study/vue-cli/pure-cli/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <div>
+      <button @click="statusIndex">Index</button>
+      <button @click="statusLunch">Lunch</button>
+      <button @click="statusLotto">Lotto</button>
+    </div>
+    <Index v-if="status === 'index'"/>
+    <!-- lunch-menu === lunchMenu === LunchMenu -->
+    <!-- lotto === Lotto -->
+    <Lunch v-if="status === 'lunch'"/>
+    <Lotto v-if="status === 'lotto'"/>
+  </div>
+</template>
+
+<script>
+import Index from './components/Index.vue'
+import Lunch from './components/Lunch.vue'
+import Lotto from './components/Lotto.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Index,
+    Lunch,
+    Lotto,
+  },
+  data: function(){
+    return {
+      status: 'index',
+    }
+  },
+  methods: {
+    statusLunch(){
+      this.status = 'lunch'
+    },
+    statusLotto(){
+      this.status = 'lotto'
+    },
+    statusIndex(){
+      this.status = 'index'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue/vue_study/vue-cli/pure-cli/src/components/Lunch.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Lunch</h1>
+    <li v-for="menu in menus" v-bind:key="menu">{{ menu }}</li>
+    <button @click="pick">선택</button>
+    <p>{{ choice }}</p>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+export default {
+  data: function(){
+    return {
+      menus: ['짜장', '짬뽕', '김밥', '라면'],
+      choice: '',
+    }
+  },
+  methods: {
+    pick(){
+      this.choice = _.sample(this.menus)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
