5a40039908037f45f7a407e2362666e295480de9
athletejuan <vanillasky84.0627@gmail.com>
2020-05-31 19:57:22 +0900

    Vue_Day04_exercise(router)

---
+++ Vue/vue_study/vue-cli/todo-router/src/components/TodoInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <input type="text" v-model="todo" @keypress.enter="createTodo">
+    <button @click="createTodo">등록</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoInput',
+  data() {
+    return {
+      todo: ''
+    }
+  },
+  methods: {
+    createTodo() {
+      if (this.todo.trim()) {
+        this.$emit('create-todo', {
+          id: Date.now(),
+          content: this.todo,
+          isCompleted: false,
+        })
+        this.todo = ''
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
