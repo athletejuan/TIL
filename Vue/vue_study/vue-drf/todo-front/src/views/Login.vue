ef61ea80fd94ad25ec8474388b4195b6fbb90ce9
athletejuan <vanillasky84.0627@gmail.com>
2020-06-04 01:02:24 +0900

    Vue-drf_01_Vue_composition

---
+++ Vue/vue_study/vue-drf/todo-front/src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login">
+    <h1>로그인 페이지입니다.</h1>
+    <LoginForm/>
+  </div>
+</template>
+
+<script>
+import LoginForm from '@/components/LoginForm.vue'
+
+export default {
+  name: 'Login',
+  components: {
+    LoginForm,
+  }
+}
+</script>
+
+<style>
+
+</style>
