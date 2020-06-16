d7a97a7388ff53c03bf252d1c82ae96db1f4521a
athletejuan <vanillasky84.0627@gmail.com>
2020-06-17 08:53:00 +0900

    AWS_ElasticBeanstalk_deploysetting

--- Web/04_django/Ex/EXERCISE4/exercise4/settings.py
+++ Web/04_django/Ex/EXERCISE4/exercise4/settings.py
@@ -1,4 +1,5 @@
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -8,12 +9,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!@u=4%3s6^8pvt_s8vgc1c(o=&ubeab$are_=fte6@^oqghj^!'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['juanex-dev.ap-northeast-2.elasticbeanstalk.com']
 
 
 # Application definition
@@ -109,3 +110,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
