f40d025a631a3b208f79f7423977a2a2e656c4b9
athletejuan <vanillasky84.0627@gmail.com>
2020-06-20 14:18:10 +0900

    REST_API_with_TMDB_model

--- Web/04_django/Corona/MOVIE_API/movies/models.py
+++ Web/04_django/Corona/MOVIE_API/movies/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=20)
+        
+class Movie(models.Model):
+    title = models.CharField(max_length=30)
+    audience = models.IntegerField()
+    poster_url = models.TextField()
+    description = models.TextField()
+    genre = models.ForeignKey(Genre, related_name='movies', on_delete=models.CASCADE)
+    
+class Score(models.Model):
+    content = models.CharField(max_length=140)
+    score = models.IntegerField()
+    movie = models.ForeignKey(Movie, related_name='scores', on_delete=models.CASCADE)
