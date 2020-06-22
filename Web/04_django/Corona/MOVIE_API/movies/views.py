ee3ce5be01296dcafb17e0bb1f3ad1d103964c89
athletejuan <vanillasky84.0627@gmail.com>
2020-06-22 17:43:12 +0900

    REST_API_with_TMDB_drf_views

--- Web/04_django/Corona/MOVIE_API/movies/views.py
+++ Web/04_django/Corona/MOVIE_API/movies/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Movie
+from rest_framework.decorators import api_view
 
+@api_view(['GET'])
 def movies_list(request):
     movies = Movie.objects.all()
