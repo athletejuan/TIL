3f230dfb425c16eedda3506ceb0b762c3d6a2196
athletejuan <vanillasky84.0627@gmail.com>
2020-07-13 01:22:51 +0900

    Flask_Chatbot_remind_pythonanywhere

--- Python/FLASK_EX/4th_CHATBOT/set_webhook.py
+++ Python/FLASK_EX/4th_CHATBOT/set_webhook.py
@@ -4,8 +4,10 @@ from decouple import config
 token = config('TOKEN')
 app_url = f'https://api.telegram.org/bot{token}'
 ngrok_url = '###'
+python_anywhere_url = 'https://juan.pythonanywhere.com/'
 
 ngrok_setting = f'{app_url}/setWebhook?url={ngrok_url}/telegram'
+anywhere_setting = f'{app_url}/setWebhook?url={python_anywhere_url}/telegram'
 
-res = requests.get(ngrok_setting)
+res = requests.get(anywhere_setting)
 # print(res.text)
