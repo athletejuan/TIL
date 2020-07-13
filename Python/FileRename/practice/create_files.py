224083a3dbbe8df514f6e8246dff9d17875b46e5
athletejuan <vanillasky84.0627@gmail.com>
2020-07-14 00:51:47 +0900

    create_files_win/mac

--- Python/FileRename/practice/create_files.py
+++ Python/FileRename/practice/create_files.py
@@ -7,6 +7,10 @@ family = ['김','이','박','최','황','오','강','한','제갈','하','정','
 given = ['길동','준','민준','소미','수진','지은','동해','민태','준호','세정','지훈','성우','성원']
 
 for i in range(500):
-    cmd = f'touch {i+1}_{random.choice(family)}{random.choice(given)}.txt'
+    # window
     # cmd = f'echo.> {i+1}_{random.choice(family)}{random.choice(given)}.txt'
+    # window & mac
+    cmd = f'echo> {i+1}_{random.choice(family)}{random.choice(given)}.txt'
+    # cmd = f'touch {i+1}_{random.choice(family)}{random.choice(given)}.txt'
+    # print(cmd)
     os.system(cmd)
