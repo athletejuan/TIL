57460e43c4bc3c45c066c0b572775287accab7d7
athletejuan <vanillasky84.0627@gmail.com>
2020-07-16 00:29:05 +0900

    StartCamp_Day01

--- Python/api_basic/browser.py
+++ Python/api_basic/browser.py
@@ -1,5 +1,11 @@
 import webbrowser
 
-webbrowser.open('https://www.google.com')
-webbrowser.open_new('https://www.naver.com')
-webbrowser.open_new_tab('https://www.daum.net')
+# webbrowser.open('https://www.google.com')
+# webbrowser.open_new('https://www.naver.com')
+# webbrowser.open_new_tab('https://www.daum.net')
+
+heroes = ['iron man', 'batman', 'wonder woman', 'black widow']
+
+for hero in heroes:
+    print(hero)
+    webbrowser.open('https://search.naver.com/search.naver?query=' + hero)
