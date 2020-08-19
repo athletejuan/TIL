62fb69996c752d90b4a9269a4644a0e6700e1a8e
athletejuan <vanillasky84.0627@gmail.com>
2020-08-20 00:02:08 +0900

    Django_Day04_workshop

---
+++ Web/SS4th/7th_Django/CRUD/articles/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render, redirect
+from .models import Article
+
+def index(request):
+    # articles = Article.objects.all()[::-1]    # 파이썬이 변경
+    articles = Article.objects.order_by('-pk')  # DB에서 변경
+    context = {
+        'articles': articles
+    }
+    return render(request, 'articles/index.html', context)
+
+
+def new(request):
+    return render(request, 'articles/new.html')
+
+
+def create(request):
+    article = Article()
+    article.title = request.POST.get('title')
+    article.content = request.POST.get('content')
+    article.save()
+    return redirect('articles:detail', article.pk)
+
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article,
+    }
+    return render(request, 'articles/detail.html', context)
+
+
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article
+    }
+    return render(request, 'articles/edit.html', context)
+
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.title = request.POST.get('title')
+    article.content = request.POST.get('content')
+    article.save()
+    return redirect('articles:detail', article.pk)
+
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.method == 'POST':
+        article.delete()
+        return redirect('articles:index')
+    else:
+        return redirect('articles:detail', article.pk)
