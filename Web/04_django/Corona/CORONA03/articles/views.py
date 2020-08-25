0d48730a635ebe5fa37a3b3336eb1207e16dd336
athletejuan <vanillasky84.0627@gmail.com>
2020-08-25 09:54:01 +0900

    Django_ver3.1_check01

---
+++ Web/04_django/Corona/CORONA03/articles/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from .models import Article
+
+def index(request):
+    articles = Article.objects.order_by('-pk')
+    context = {
+        'articles': articles
+    }
+    return render(request, 'articles/index.html', context)
+
+
+def new(request):
+    if request.method == 'POST':
+        article = Article()
+        article.title = request.POST.get('title')
+        article.content = request.POST.get('content')
+        article.save()
+        return redirect('articles:detail', article.pk)
+    else:
+        return render(request, 'articles/new.html')
+
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article
+    }
+    return render(request, 'articles/detail.html', context)
