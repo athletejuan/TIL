2efdeef5ba0923cd9d82b4bc2841a6b846f5dce1
athletejuan <vanillasky84.0627@gmail.com>
2020-09-15 00:15:21 +0900

    Django_Day06_workshop

--- Web/SS4th/7th_Django/CRUD/articles/models.py
+++ Web/SS4th/7th_Django/CRUD/articles/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import Thumbnail
 
 class Article(models.Model):
     title = models.CharField(max_length=20)
     content = models.TextField()
+    image = ProcessedImageField(
+        blank=True,
+        processors=[Thumbnail(200,300)],
+        format='JPEG',
+        options={'quality': 90},
+        upload_to='%Y/%m/%d',
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
