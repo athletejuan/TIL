98b49034ce1977a3bf5e3baeb1716b5057df1d8c
athletejuan <vanillasky84.0627@gmail.com>
2020-10-29 09:12:19 +0900

    Vue_youtube_2010

---
+++ Vue/vue_study/youtube-browser-cp/ex08/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <SearchBar @inputChange="onInputChange"></SearchBar>
+    <VideoList :videos="videos"></VideoList>
+    {{ videos.length }}
+  </div>
+</template>
+
+<script>
+const API_KEY = process.env.VUE_APP_YOUTUBE_API_KEY
+const API_URL = 'https://www.googleapis.com/youtube/v3/search'
+import axios from 'axios'
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList'
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      videos: [],
+    }
+  },
+  components: {
+    SearchBar,
+    VideoList,
+  },
+  methods: {
+    onInputChange (inputValue) {
+      axios.get(API_URL, {
+        params: {
+          key: API_KEY,
+          type: 'video',
+          part: 'snippet',
+          q: inputValue
+        }
+      })
+      .then((response) => {
+        this.videos = response.data.items
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Vue/vue_study/youtube-browser-cp/ex08/src/components/VideoList.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    VideoList
+    {{ videos.length }}
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'VideoList',
+  props: {
+    videos: Array,
+  }
+}
+</script>
+
+<style>
+
+</style>
