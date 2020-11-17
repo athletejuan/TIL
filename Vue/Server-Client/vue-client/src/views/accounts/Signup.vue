3858c8c7871c237280a9f5adcfe7eb9ba19b67ec
athletejuan <vanillasky84.0627@gmail.com>
2020-11-17 22:56:33 +0900

    SS4th_Lecture

--- Vue/Server-Client/vue-client/src/views/accounts/Signup.vue
+++ Vue/Server-Client/vue-client/src/views/accounts/Signup.vue
@@ -11,9 +11,9 @@
     </div>
     <div>
       <label for="passwordConfirmation">비밀번호 확인: </label>
-      <input type="password" id="passwordConfirmation" v-model="credentials.passwordConfirmation" @keydown="signup(credentials)">
+      <input type="password" id="passwordConfirmation" v-model="credentials.passwordConfirmation" @keydown="signup">
     </div>
-    <button @click="signup(credentials)">회원가입</button>
+    <button @click="signup">회원가입</button>
   </div>
 </template>
 
