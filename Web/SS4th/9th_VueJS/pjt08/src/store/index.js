18348d4437fb3ddab6f8bf80558c9835dac3da65
athletejuan <vanillasky84.0627@gmail.com>
2020-11-26 00:01:43 +0900

    editMovieTitle&localStorage

--- Web/SS4th/9th_VueJS/pjt08/src/store/index.js
+++ Web/SS4th/9th_VueJS/pjt08/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
@@ -50,5 +51,8 @@ export default new Vuex.Store({
     
   },
   modules: {
-  }
+  },
+  plugins: [
+    createPersistedState(),
+  ]
 })
