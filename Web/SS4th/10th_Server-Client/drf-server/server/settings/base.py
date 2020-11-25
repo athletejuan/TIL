5d9734e21388654ad101260b33d9a1d5f10b099a
athletejuan <vanillasky84.0627@gmail.com>
2020-11-26 01:22:44 +0900

    drf-server_deploytest

--- Web/SS4th/10th_Server-Client/drf-server/server/settings/base.py
+++ Web/SS4th/10th_Server-Client/drf-server/server/settings/base.py
@@ -114,6 +114,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 CORS_ORIGIN_ALLOW_ALL = True
 
 JWT_AUTH = {
---
+++ Web/SS4th/10th_Server-Client/drf-server/server/settings/production.py
@@ -0,0 +1,7 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    13.124.74.202
+]
