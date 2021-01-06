5a65887fc394ab4a2bee2447ed02c39310225baa
athletejuan <vanillasky84.0627@gmail.com>
2021-01-07 00:25:29 +0900

    find_numbers

--- Algorithm/BOJ/24_binary_search/1920.py
+++ Algorithm/BOJ/24_binary_search/1920.py
@@ -6,18 +6,16 @@ M = int(sys.stdin.readline())
 Ms = sys.stdin.readline().split()
 
 for i in range(M):
-    breaker = True
     left = 0
     right = N-1
     while left <= right:
         mid = (left + right)//2
         if Ms[i] == Ns[mid]:
             print(1)
-            breaker = False
             break
         elif Ms[i] > Ns[mid]:
             left = mid+1
         else:
             right = mid-1
-    if breaker:
+    else:
         print(0)
