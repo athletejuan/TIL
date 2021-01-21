5ccb351d5e6aa1e71c94b2c5bfc01976cea6716f
athletejuan <vanillasky84.0627@gmail.com>
2021-01-22 00:31:48 +0900

    palindrome_check_recursive

--- Algorithm/BOJ/08_string/10988.py
+++ Algorithm/BOJ/08_string/10988.py
@@ -1,5 +1,16 @@
 word = input()
-if word == word[::-1]:
-    print(1)
-else:
-    print(0)
+# if word == word[::-1]:
+#     print(1)
+# else:
+#     print(0)
+
+
+def palindrome_check(word):
+    if len(word) < 2:
+        return 1
+    if word[0] == word[-1]:
+        return palindrome_check(word[1:-1])
+    else:
+        return 0
+
+print(palindrome_check(word))
