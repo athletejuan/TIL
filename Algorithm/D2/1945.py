d2dd3afe499278f29e2e51b8b51c04f2d6fc0a5e
athletejuan <vanillasky84.0627@gmail.com>
2021-02-10 15:45:42 +0900

    SWEA_D2_part02_prime_factor

--- Algorithm/D2/1945.py
+++ Algorithm/D2/1945.py
@@ -1,21 +1,11 @@
 T = int(input())
 
 for test_case in range(1, T+1):
-    number = int(input())
-    a, b, c, d, e = 0, 0, 0, 0, 0
-    while number % 2 == 0:
-        number /= 2
-        a += 1
-    while number % 3 == 0:
-        number /= 3
-        b += 1
-    while number % 5 == 0:
-        number /= 5
-        c += 1
-    while number % 7 == 0:
-        number /= 7
-        d += 1
-    while number % 11 == 0:
-        number /= 11
-        e += 1
-    print(f'#{test_case} {a} {b} {c} {d} {e}')
+    N = int(input())
+    prime = [2, 3, 5, 7, 11]
+    count = [0] * 5
+    for i in range(5):
+        while not N % prime[i]:
+            N /= prime[i]
+            count[i] += 1
+    print(f'#{test_case} {" ".join(map(str, count))}')
