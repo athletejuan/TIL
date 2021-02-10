04d6410d1fce6280498800c19407a5bd0dff0017
athletejuan <vanillasky84.0627@gmail.com>
2021-02-10 15:26:05 +0900

    SWEA_D2_array_max_couple

--- Algorithm/D2/1959.py
+++ Algorithm/D2/1959.py
@@ -2,15 +2,16 @@ T = int(input())
 
 for test_case in range(1, T+1):
     N,M = map(int, input().split())
-    sum_two = []
     num1 = list(map(int, input().split()))
     num2 = list(map(int, input().split()))
-    for i in range(abs(N-M)+1):
-        two = 0
-        for j in range(i, min(N,M)+i):
-            if N < M:
-                two += num1[j-i] * num2[j]
-            else:
-                two += num1[j] * num2[j-i]
-        sum_two.append(two)
-    print(f'#{test_case} {max(sum_two)}')
+    if N < M:
+        N,M,num1,num2 = M,N,num2,num1
+    for i in range(N-M+1):
+        couple = 0
+        for j in range(i, i+M):
+            couple += num1[j] * num2[j-i]
+        if not i:
+            max_couple = couple
+        elif couple > max_couple:
+            max_couple = couple
+    print(f'#{test_case} {max_couple}')
