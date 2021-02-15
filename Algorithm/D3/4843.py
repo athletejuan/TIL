1f4e0eae8f9621f4c8df80eeda4dcefbb1429cf2
athletejuan <vanillasky84.0627@gmail.com>
2021-02-15 17:12:30 +0900

    SWEA_PS_LIST2_special_sort_recap

--- Algorithm/D3/4843.py
+++ Algorithm/D3/4843.py
@@ -1,14 +1,30 @@
 T = int(input())
 
-for test_case in range(1, T+1):
-    num = int(input())
-    arr = ['-']+sorted(list(map(int, input().split())))
-    new = []
-    s = -1
+for tc in range(1, T+1):
+    N = int(input())
+    nums = list(map(int, input().split()))
     for i in range(10):
-        new.append(arr[s])
-        if s>0:
-            s -= i+2
-        else:
-            s += i+2
-    print(f'#{test_case} {" ".join(list(map(str, new)))}')
+        minmax = i
+        for j in range(i+1, N):
+            if i%2:
+                if nums[minmax] > nums[j]:
+                    minmax = j
+            else:
+                if nums[minmax] < nums[j]:
+                    minmax = j
+        nums[i],nums[minmax] = nums[minmax],nums[i]
+    print(f'#{tc} {" ".join(map(str, nums[:10]))}')
+
+
+# for test_case in range(1, T+1):
+#     num = int(input())
+#     arr = ['-']+sorted(list(map(int, input().split())))
+#     new = []
+#     s = -1
+#     for i in range(10):
+#         new.append(arr[s])
+#         if s>0:
+#             s -= i+2
+#         else:
+#             s += i+2
+#     print(f'#{test_case} {" ".join(list(map(str, new)))}')
