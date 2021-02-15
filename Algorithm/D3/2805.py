acee0735d7d5debdf0e67a47ebba1c4ce3729060
athletejuan <vanillasky84.0627@gmail.com>
2021-02-15 18:18:26 +0900

    SWEA_D3_harvest

--- Algorithm/D3/2805.py
+++ Algorithm/D3/2805.py
@@ -1,16 +1,30 @@
 T = int(input())
 
-for test_case in range(1, T+1):
-    n = int(input())
-    answer = 0
-    for i in range(n):
-        row = input()
-        str_list = []
-        for j in row:
-            str_list.append(j)
-        num_list = list(map(int, str_list))
-        if i > (n//2):
-            answer += sum(num_list[((n//2)-(i-(2*(i-(n//2))))):((n//2)+(i+1)-(2*(i-(n//2))))])
+for tc in range(1, T+1):
+    N = int(input())
+    harvest = 0
+    for i in range(N):
+        row = list(map(int, list(input())))
+        if i <= N//2:
+            harvest += sum(row[N//2-i:N//2+i+1])
         else:
-            answer += sum(num_list[((n//2)-i):((n//2)+(i+1))])
-    print(f'#{test_case} {answer}')
+            harvest += sum(row[-(N//2)+i:N-(i-N//2)])
+    print(f'#{tc} {harvest}')
+
+
+# 1st try
+
+# for test_case in range(1, T+1):
+#     n = int(input())
+#     answer = 0
+#     for i in range(n):
+#         row = input()
+#         str_list = []
+#         for j in row:
+#             str_list.append(j)
+#         num_list = list(map(int, str_list))
+#         if i > (n//2):
+#             answer += sum(num_list[((n//2)-(i-(2*(i-(n//2))))):((n//2)+(i+1)-(2*(i-(n//2))))])
+#         else:
+#             answer += sum(num_list[((n//2)-i):((n//2)+(i+1))])
+#     print(f'#{test_case} {answer}')
