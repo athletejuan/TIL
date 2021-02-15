431d3de948f3f39eb0a4039b8153711be05ee821
athletejuan <vanillasky84.0627@gmail.com>
2021-02-15 17:13:13 +0900

    SWEA_PS_LIST2_sum_of_subsets

--- Algorithm/D3/4837.py
+++ Algorithm/D3/4837.py
@@ -1,18 +1,14 @@
 T = int(input())
 
-arr = [1,2,3,4,5,6,7,8,9,10,11,12]
-for test_case in range(1, T+1):
+arr = [i for i in range(1, 13)]
+for tc in range(1, T+1):
     N,K = map(int, input().split())
-    div = []
     cnt = 0
     for i in range(1 << 12):
-        sub_div = []
+        sub = []
         for j in range(12):
             if i & (1 << j):
-                sub_div.append(arr[j])
-        div.append(sub_div)
-    
-    for k in div:
-        if len(k) == N and sum(k) == K:
+                sub.append(arr[j])
+        if len(sub) == N and sum(sub) == K:
             cnt += 1
-    print(f'#{test_case} {cnt}')
+    print(f'#{tc} {cnt}')
