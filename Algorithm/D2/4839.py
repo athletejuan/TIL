2da9b38d33749e9189e6142b9a21b4689a787070
athletejuan <vanillasky84.0627@gmail.com>
2021-02-15 17:14:04 +0900

    SWEA_PS_LIST2_binary_search

--- Algorithm/D2/4839.py
+++ Algorithm/D2/4839.py
@@ -1,30 +1,33 @@
 T = int(input())
 
-for test_case in range(1, T+1):
-    P,A,B = map(int, input().split())
-    cA = 0
-    cB = 0
-    c = int((P+1)/2)
-    l, r = 1, P
-    while c != A:
-        if c < A:
-            l = c
+for tc in range(1, T+1):
+    P,Pa,Pb = map(int, input().split())
+    A,B = 0,0
+    L,R = 1,P
+    while L <= R:
+        mid = (L+R)//2
+        A += 1
+        if mid == Pa:
+            break
+        elif mid > Pa:
+            R = mid-1
         else:
-            r = c
-        c = int((l+r)/2)
-        cA += 1
-    c = int((P+1)/2)
-    l, r = 1, P
-    while c != B:
-        if c < B:
-            l = c
+            L = mid+1
+    L,R = 1,P
+    while L <= R:
+        mid = (L+R)//2
+        B += 1
+        if mid == Pb:
+            if A < B:
+                print(f'#{tc} A')
+                break
+            elif A > B:
+                print(f'#{tc} B')
+                break
+            else:
+                print(f'#{tc} 0')
+                break
+        elif mid > Pb:
+            R = mid-1
         else:
-            r = c
-        c = int((l+r)/2)
-        cB += 1
-    if cA < cB:
-        print(f'#{test_case} A')
-    elif cA > cB:
-        print(f'#{test_case} B')
-    else:
-        print(f'#{test_case} 0')
+            L = mid+1
