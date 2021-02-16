70fcc42db2a9cf67338cf8984fad4c6d2c62508c
athletejuan <vanillasky84.0627@gmail.com>
2021-02-17 00:41:25 +0900

    SWEA_PS_LIST2_color_STUDY

--- Algorithm/D2/4836.py
+++ Algorithm/D2/4836.py
@@ -17,3 +17,37 @@ for test_case in range(1, T+1):
         if j in blue:
             count += 1
     print(f'#{test_case} {count}')
+
+
+# Tony
+# import sys
+# sys.stdin = open('input.txt', 'r')
+
+
+# def paint_box(r1, c1, r2, c2, color_number):
+#     for row in range(r1, r2 + 1):
+#         for col in range(c1, c2 + 1):
+#             field[row][col] += color_number
+
+
+# def calculated_result():
+#     result = 0
+#     for row_data in field:
+#         for col_data in row_data:
+#             if col_data == 3:
+#                 result += 1
+#     return result
+
+
+# T = int(input())
+
+# for t in range(1, T+1):
+#     N = int(input())
+#     field = [[0] * 10 for _ in range(10)]
+#     # 1. 색칠하기
+#     for _ in range(N):
+#         r1, c1, r2, c2, color_number = map(int, input().split())
+#         paint_box(r1, c1, r2, c2, color_number)
+
+#     # 2. 색칠한 결과 계산하기
+#     print('#{} {}'.format(t, calculated_result()))
