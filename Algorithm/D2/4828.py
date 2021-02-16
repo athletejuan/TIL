b6347dd4a6a3d6d7ce8d6eb25329f8f952b8729b
athletejuan <vanillasky84.0627@gmail.com>
2021-02-16 21:38:40 +0900

    SWEA_PS_LIST1_min_max

--- Algorithm/D2/4828.py
+++ Algorithm/D2/4828.py
@@ -1,7 +1,18 @@
 T = int(input())
 
-for test_case in range(1, T+1):
-    num = input()
+for tc in range(1, T+1):
+    N = int(input())
     nums = list(map(int, input().split()))
-    sn = sorted(nums)
-    print(f'#{test_case} {sn[-1] - sn[0]}')
+    for i in range(N-1):
+        for j in range(N-i-1):
+            if nums[j] > nums[j+1]:
+                nums[j],nums[j+1] = nums[j+1],nums[j]
+    print(f'#{tc} {nums[-1] - nums[0]}')
+
+
+# 1st try
+# for test_case in range(1, T+1):
+#     num = input()
+#     nums = list(map(int, input().split()))
+#     sn = sorted(nums)
+#     print(f'#{test_case} {sn[-1] - sn[0]}')
