8c9772eb52fd7caac74767a74c6da4eb485cde35
athletejuan <vanillasky84.0627@gmail.com>
2021-02-16 22:24:33 +0900

    SWEA_PS_LIST1_num_card

--- Algorithm/D2/4834.py
+++ Algorithm/D2/4834.py
@@ -1,17 +1,32 @@
 T = int(input())
 
-for test_case in range(1, T+1):
-    num = int(input())
-    nums = input()
-    card = {}
-    count = []
-    for i in set(nums):
-        count.append(nums.count(i))
-        card[i] = nums.count(i)
-    keys = []
-    values = []
-    for key, value in card.items():
-        if value == sorted(count)[-1]:
-            keys.append(int(key))
-            values.append(int(value))
-    print(f'#{test_case} {sorted(keys)[-1]} {sorted(values)[-1]}')
+for tc in range(1, T+1):
+    N = int(input())
+    cards = [0] * 10
+    nums = list(map(int, list(input())))
+    for num in nums:
+        cards[num] += 1
+    many = 0
+    for idx, card in enumerate(cards):
+        if card >= many:
+            many = card
+            card_num = idx
+    print('#{} {} {}'.format(tc, card_num, many))
+
+
+# 1st try
+# for test_case in range(1, T+1):
+#     num = int(input())
+#     nums = input()
+#     card = {}
+#     count = []
+#     for i in set(nums):
+#         count.append(nums.count(i))
+#         card[i] = nums.count(i)
+#     keys = []
+#     values = []
+#     for key, value in card.items():
+#         if value == sorted(count)[-1]:
+#             keys.append(int(key))
+#             values.append(int(value))
+#     print(f'#{test_case} {sorted(keys)[-1]} {sorted(values)[-1]}')
