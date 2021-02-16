c66dc7374e4c5e00daa9c499656654ee87237a41
athletejuan <vanillasky84.0627@gmail.com>
2021-02-16 21:58:06 +0900

    SWEA_PS_LIST1_electric_bus

--- Algorithm/D3/4831.py
+++ Algorithm/D3/4831.py
@@ -1,20 +1,42 @@
 T = int(input())
 
-for test_case in range(1, T+1):
-    nums = list(map(int, input().split()))
+for tc in range(1, T+1):
+    K,N,M = map(int, input().split())
     stop = list(map(int, input().split()))
     start = 0
     count = 0
-    while (start + nums[0]) < nums[1]:
-        for i in range(nums[0], 0, -1):
-            if start + i in stop:
-                start += i
+
+    while start < N:
+        start += K
+        if start >= N:
+            print(f'#{tc} {count}')
+            break
+        for i in range(start, start-K, -1):
+            if i in stop:
+                start = i
                 count += 1
                 break
-            elif (start + nums[0]) > nums[1]:
-                print(f'#{test_case} {count}')
         else:
-            print(f'#{test_case} 0')
+            print(f'#{tc} 0')
             break
-    else:
-        print(f'#{test_case} {count}')
+
+
+# 1st try
+# for test_case in range(1, T+1):
+#     nums = list(map(int, input().split()))
+#     stop = list(map(int, input().split()))
+#     start = 0
+#     count = 0
+#     while (start + nums[0]) < nums[1]:
+#         for i in range(nums[0], 0, -1):
+#             if start + i in stop:
+#                 start += i
+#                 count += 1
+#                 break
+#             elif (start + nums[0]) > nums[1]:
+#                 print(f'#{test_case} {count}')
+#         else:
+#             print(f'#{test_case} 0')
+#             break
+#     else:
+#         print(f'#{test_case} {count}')
