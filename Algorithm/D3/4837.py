4541d764162b20bdc6d65aeb9fc52c4a1d3aa13e
athletejuan <vanillasky84.0627@gmail.com>
2021-02-17 00:37:48 +0900

    SWEA_PS_LIST2_sum_of_subsets_study

--- Algorithm/D3/4837.py
+++ Algorithm/D3/4837.py
@@ -12,3 +12,35 @@ for tc in range(1, T+1):
         if len(sub) == N and sum(sub) == K:
             cnt += 1
     print(f'#{tc} {cnt}')
+
+
+# Tony
+# import sys
+# sys.stdin = open('input.txt', 'r')
+
+
+# def find_subset(count, s, N, K, selected):
+#     if count == N:
+#         target_value = 0
+#         for idx, val in enumerate(selected, start=1):
+#             if val:
+#                 target_value += idx
+#         if target_value == K:
+#             return 1
+#         else:
+#             return 0
+
+#     result = 0
+#     for i in range(s, 12):
+#         selected[i] = 1
+#         result += find_subset(count+1, i+1, N, K, selected)
+#         selected[i] = 0
+#     return result
+
+
+# T = int(input())
+
+# for t in range(1, T+1):
+#     N, K = map(int, input().split())
+#     selected = [0] * 12
+#     print('#{} {}'.format(t, find_subset(0, 0, N, K, selected)))
