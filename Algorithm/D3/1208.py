9b9dcdc8c15f235b490a7c76b37437d32c950bff
athletejuan <vanillasky84.0627@gmail.com>
2021-02-17 00:33:54 +0900

    SWEA_D3_part01_flatten_recap

--- Algorithm/D3/1208.py
+++ Algorithm/D3/1208.py
@@ -1,19 +1,49 @@
-for test_case in range(1, 11):
+for tc in range(1, 11):
     count = int(input())
-    height = input()
-    h_list = sorted(list(map(int, height.split())))
+    boxes = sorted(list(map(int, input().split())))
     for i in range(count):
-        high = h_list.pop()
-        low = h_list.pop(0)
-        # gap = high - low
-        high -= 1
-        low += 1
-        h_list.append(high)
-        h_list.append(low)
-        gap = max(h_list) - min(h_list)
-        if gap <= 1:
-            print(f'#{test_case} {gap}')
+        boxes.extend([boxes.pop()-1, boxes.pop(0)+1])
+        boxes.sort()
+        gap = boxes[-1] - boxes[0]
+        if gap < 2:
+            print('#{} {}'.format(tc, gap))
             break
-        h_list.sort()
     else:
-        print(f'#{test_case} {gap}')
+        print('#{} {}'.format(tc, gap))
+
+
+# mergeSort
+# def mergeSort(x):
+#     if len(x) > 1:
+#         mid = len(x)//2
+#         L,R = x[:mid],x[mid:]
+#         mergeSort(L)
+#         mergeSort(R)
+    
+#         Li, Ri, i = 0, 0, 0
+#         while Li < len(L) and Ri < len(R):
+#             if L[Li] < R[Ri]:
+#                 x[i] = L[Li]
+#                 Li += 1
+#             else:
+#                 x[i] = R[Ri]
+#                 Ri += 1
+#             i += 1
+#         x[i:] = L[Li:] if len(L) != Li else R[Ri:]
+
+# for tc in range(1, 11):
+#     N = int(input())
+#     boxes = list(map(int, input().split()))
+#     mergeSort(boxes)
+
+#     while N:
+#         gap = boxes[-1] - boxes[0]
+#         if gap < 2:
+#             print('#{} {}'.format(tc, gap))
+#             break
+#         boxes[0] += 1
+#         boxes[-1] -= 1
+#         N -= 1
+#         mergeSort(boxes)
+#     else:
+#         print('#{} {}'.format(tc, boxes[-1] - boxes[0]))
