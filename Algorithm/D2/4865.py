543553806ed3735ec28db455f03dcc4f506e7806
athletejuan <vanillasky84.0627@gmail.com>
2021-02-17 10:25:49 +0900

    SWEA_PS_String_char_cnt

--- Algorithm/D2/4865.py
+++ Algorithm/D2/4865.py
@@ -1,11 +1,32 @@
 T = int(input())
 
-for test_case in range(1, T+1):
-    str1 = input()
-    str2 = input()
+for tc in range(1, T+1):
+    N = set(list(input()))
+    M = input()
+    str_cnt = {}
+    max_val = 0
 
-    rank = 0
-    for i in str1:
-        if str2.count(i) > rank:
-            rank = str2.count(i)
-    print(f'#{test_case} {rank}')
+    for n in N:
+        for m in M:
+            if n == m:
+                if n in str_cnt:
+                    str_cnt[n] += 1
+                else:
+                    str_cnt[n] = 1
+
+    for value in str_cnt.values():
+        if value > max_val:
+            max_val = value
+    print('#{} {}'.format(tc, max_val))
+
+
+# 1st try
+# for test_case in range(1, T+1):
+#     str1 = input()
+#     str2 = input()
+
+#     rank = 0
+#     for i in str1:
+#         if str2.count(i) > rank:
+#             rank = str2.count(i)
+#     print(f'#{test_case} {rank}')
