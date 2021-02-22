0863b585313d96986a869ac08841361dcb96e390
athletejuan <vanillasky84.0627@gmail.com>
2021-02-22 22:36:52 +0900

    SWEA_PS_Stack1_rectangular_paper_memoization

--- Algorithm/D2/4869.py
+++ Algorithm/D2/4869.py
@@ -1,6 +1,6 @@
 T = int(input())
 
-for test_case in range(1, T+1):
+for tc in range(1, T+1):
     N = int(input())
     p = 1
     t = N//10
@@ -9,4 +9,26 @@ for test_case in range(1, T+1):
             p = (p*2) - 1
         else:
             p = (p*2) + 1
-    print(f'#{test_case} {p}')
+    print(f'#{tc} {p}')
+
+
+# memoization(more fast)
+# T = int(input())
+# Ns = []
+# max_N = 0
+# for _ in range(T):
+#     N = int(input())
+#     Ns.append(N//10)
+#     if N//10 > max_N:
+#         max_N = N//10
+
+# t = max_N
+# memo = [1] + [0] * (t-1)
+# for i in range(1, t):
+#     if i%2:
+#         memo[i] = (memo[i-1]*2) + 1
+#     else:
+#         memo[i] = (memo[i-1]*2) - 1
+
+# for idx,j in enumerate(Ns):
+#     print('#{} {}'.format(idx+1, memo[j-1]))
