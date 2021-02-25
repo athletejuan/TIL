b183de6735190d82110118b9d88ec03217bf4a3d
athletejuan <vanillasky84.0627@gmail.com>
2021-02-25 17:34:53 +0900

    SWEA_D3_part01_flatten_recap

--- Algorithm/D3/1208.py
+++ Algorithm/D3/1208.py
@@ -1,15 +1,28 @@
 for tc in range(1, 11):
     count = int(input())
-    boxes = sorted(list(map(int, input().split())))
+    boxes = list(map(int, input().split()))
     for i in range(count):
-        boxes.extend([boxes.pop()-1, boxes.pop(0)+1])
-        boxes.sort()
-        gap = boxes[-1] - boxes[0]
-        if gap < 2:
-            print('#{} {}'.format(tc, gap))
-            break
-    else:
-        print('#{} {}'.format(tc, gap))
+        max_idx, min_idx = 0, 0
+        for idx, box in enumerate(boxes):
+            if not idx:
+                max_val, min_val = box, box
+            else:
+                if max_val < box:
+                    max_idx, max_val = idx, box
+                if min_val > box:
+                    min_idx, min_val = idx, box
+        boxes[max_idx] -= 1
+        boxes[min_idx] += 1
+    for j in range(100):
+        if not j:
+            top, bottom = boxes[0], boxes[0]
+        else:
+            if boxes[j] > top:
+                top = boxes[j] 
+            if boxes[j] < bottom:
+                bottom = boxes[j] 
+    print('#{} {}'.format(tc, top - bottom))
+
 
 
 # mergeSort
@@ -47,3 +60,18 @@ for tc in range(1, 11):
 #         mergeSort(boxes)
 #     else:
 #         print('#{} {}'.format(tc, boxes[-1] - boxes[0]))
+
+
+# 1st try
+# for tc in range(1, 11):
+#     count = int(input())
+#     boxes = sorted(list(map(int, input().split())))
+#     for i in range(count):
+#         boxes.extend([boxes.pop()-1, boxes.pop(0)+1])
+#         boxes.sort()
+#         gap = boxes[-1] - boxes[0]
+#         if gap < 2:
+#             print('#{} {}'.format(tc, gap))
+#             break
+#     else:
+#         print('#{} {}'.format(tc, gap))
