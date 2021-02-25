f50598c2a53b456b562ae095cebf3a248064a5d7
athletejuan <vanillasky84.0627@gmail.com>
2021-02-25 16:34:30 +0900

    SWEA_D2_sudoku_check_recap

--- Algorithm/D2/1974.py
+++ Algorithm/D2/1974.py
@@ -1,137 +1,88 @@
 T = int(input())
 
-for test_case in range(1, T+1):
-    sudoku = []
-    sum_cube = []
+def row_col(x):
+    row_sum, col_sum, dup_row, dup_col = 0, 0, [], []
+    for i in range(9):
+        # 행,열 방향 합산 45 확인
+        row_sum += sudoku[x][i]
+        col_sum += sudoku[i][x]
+        # 행,열 방향 중복 확인
+        if sudoku[x][i] not in dup_row:
+            dup_row.append(sudoku[x][i])
+        if sudoku[i][x] not in dup_col:
+            dup_col.append(sudoku[i][x])
+    return 1 if len(dup_row) == len(dup_col) == 9 and row_sum == col_sum == 45 else 0
+
+def cube(x, y):
+    # 3x3 큐브안의 항목들의 중복 여부와 합이 45인지 확인
+    cube_sum, dup_cube = 0, []
+    for i in range(x, x+3):
+        for j in range(y, y+3):
+            cube_sum += sudoku[i][j]
+            # 중복 여부 확인
+            if sudoku[i][j] not in dup_cube:
+                dup_cube.append(sudoku[i][j])
+    return 1 if cube_sum == 45 and len(dup_cube) == 9 else 0
+
+for tc in range(1, T+1):
+    sudoku = [list(map(int, input().split())) for _ in range(9)]
     result = 1
-    for i in range(3):
-        cube = []
-        for j in range(3):
-            row = list(map(int, input().split()))
-            sudoku.append(row)
-            if sum(row) != 45:
-                result = 0
-            for k in range(3):
-                cube.append(sum(row[3*k:3*(k+1)]))
-        sum_cube.append(cube)
-        sc = 0
-        for b in range(3):
-            sc += sum_cube[i][(3*b)+i]
-        if sc != 45:
+    # 행,열 합산 및 중복 여부 확인
+    for i in range(9):
+        if not row_col(i):
             result = 0
-    for l in range(9):
-        cs = 0
-        for m in range(9):
-            cs += sudoku[m][l]
-        if cs != 45:
-            result = 0
-    if result:
-        for a in range(9):
-            num_check = []
-            for c in range(9):
-                if len(set(sudoku[c])) != 9:
-                    result = 0
-                num_check.append(sudoku[c][a])
-            if len(set(num_check)) != 9:
+    # 3x3 큐브의 합산 및 중복 여부 확인
+    for j in range(3):
+        for k in range(3):
+            if not cube(3*j, 3*k):
                 result = 0
-        for d in range(3):
-            for e in range(3):
-                tri = []
-                for f in range(3):
-                    for g in range(3):
-                        tri.append(sudoku[(d*3)+f][(3*e)+g])
-                if len(set(tri)) != 9:
-                    result = 0
-    print(f'#{test_case} {result}')
+    print('#{} {}'.format(tc, result))
+    
 
 
+# 1st try
+# T = int(input())
 
 # for test_case in range(1, T+1):
 #     sudoku = []
+#     sum_cube = []
+#     result = 1
 #     for i in range(3):
 #         cube = []
 #         for j in range(3):
 #             row = list(map(int, input().split()))
 #             sudoku.append(row)
 #             if sum(row) != 45:
-#                 print(f'#{test_case} 0')
-#                 break
-#             t = []
+#                 result = 0
 #             for k in range(3):
-#                 t.append(sum(row[(3*k):((3*k)+3)]))
-#             cube.append(t[(3*j)+i])
-#         if sum(cube) != 45:
-#             print(f'#{test_case} 0')
-#             break
-#     for l in range(9):
-#         col = []
-#         for m in range(9):
-#             col.append(sudoku[m][l])
-#         if sum(col) != 45:
-#             print(f'#{test_case} 0')
-#             break
-#     else:
-#         print(f'#{test_case} 1')
-
-
-# for test_case in range(1, T+1):
-#     sudoku = []
-#     for i in range(9):
-#         row = list(map(int, input().split()))
-#         sudoku.append(row)
-#         if sum(row) != 45:
-#             print(f'#{test_case} 0')
-#             break
-#     for j in range(9):
-#         col = []
-#         for k in range(9):
-#             col.append(sudoku[j][k])
-#         if sum(col) != 45:
-#             print(f'#{test_case} 0')
-#             break
-#     t_cube = []
-#     for a in range(3):  
-#         for b in range(3):
-#             cube = []
-#             for c in range(3):
-#                 for d in range(3):
-#                     cube.append(sudoku[(a*3)+c][(3*b)+d])
-#             t_cube.append(cube)
-#     for e in t_cube:
-#         if sum(e) != 45:
-#             print(f'#{test_case} 0')
-#             break
-#     else:
-#         print(f'#{test_case} 1')
-
-
-
-
-# for test_case in range(1, T+1):
-#     sudoku = []
-#     t_cube = []
-#     result = 1
-#     for i in range(9):
-#         row = list(map(int, input().split()))
-#         sudoku.append(row)
-#         if sum(row) != 45:
-#             result = 0
-#         cube = []
-#         for j in range(3):
-#             cube.append(sum(row[(3*j):(3*j)+3]))
-#         t_cube.append(cube)
-#     pprint(t_cube)
-#     for k in range(3):
+#                 cube.append(sum(row[3*k:3*(k+1)]))
+#         sum_cube.append(cube)
 #         sc = 0
-#         for l in range(3):
-#             for m in range(3):
-#                 sc += t_cube[m+(3*k)][l]
-#                 if sc != 45:
-#                     result = 0
-#     for a in range(9):
-#         col = []
-#         for b in range(9):
-#             col.append(sudoku[a][b])
-#         if sum(col) != 45:
+#         for b in range(3):
+#             sc += sum_cube[i][(3*b)+i]
+#         if sc != 45:
 #             result = 0
+#     for l in range(9):
+#         cs = 0
+#         for m in range(9):
+#             cs += sudoku[m][l]
+#         if cs != 45:
+#             result = 0
+#     if result:
+#         for a in range(9):
+#             num_check = []
+#             for c in range(9):
+#                 if len(set(sudoku[c])) != 9:
+#                     result = 0
+#                 num_check.append(sudoku[c][a])
+#             if len(set(num_check)) != 9:
+#                 result = 0
+#         for d in range(3):
+#             for e in range(3):
+#                 tri = []
+#                 for f in range(3):
+#                     for g in range(3):
+#                         tri.append(sudoku[(d*3)+f][(3*e)+g])
+#                 if len(set(tri)) != 9:
+#                     result = 0
 #     print(f'#{test_case} {result}')
