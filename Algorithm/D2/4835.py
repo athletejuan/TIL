22c3a59f01cd7a8e7fc839dfb45c392e02519179
athletejuan <vanillasky84.0627@gmail.com>
2021-02-25 17:33:49 +0900

    SWEA_PS_LIST1_prefix_sum

--- Algorithm/D2/4835.py
+++ Algorithm/D2/4835.py
@@ -3,11 +3,31 @@ T = int(input())
 for tc in range(1, T+1):
     N,M = map(int, input().split())
     arr = list(map(int, input().split()))
-    parts = []
     for i in range(N-M+1):
         part = 0
         for j in range(i, i+M):
             part += arr[j]
-        parts.append(part)
-    parts.sort()
-    print('#{} {}'.format(tc, parts[-1] - parts[0]))
+        if not i:
+            top, bottom = part, part
+        else:
+            if part > top:
+                top = part
+            if part < bottom:
+                bottom = part
+    print('#{} {}'.format(tc, top - bottom))
+
+
+# 1st try
+# T = int(input())
+
+# for tc in range(1, T+1):
+#     N,M = map(int, input().split())
+#     arr = list(map(int, input().split()))
+#     parts = []
+#     for i in range(N-M+1):
+#         part = 0
+#         for j in range(i, i+M):
+#             part += arr[j]
+#         parts.append(part)
+#     parts.sort()
+#     print('#{} {}'.format(tc, parts[-1] - parts[0]))
