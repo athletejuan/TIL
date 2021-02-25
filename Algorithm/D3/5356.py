a2801c5816c1c0d51bedfb1c47c0b39c83dc5c94
athletejuan <vanillasky84.0627@gmail.com>
2021-02-25 16:20:43 +0900

    SWEA_D3_vertical

--- Algorithm/D3/5356.py
+++ Algorithm/D3/5356.py
@@ -3,14 +3,16 @@ T = int(input())
 for tc in range(1, T+1):
     l = 0
     arr = []
-    col = []
+    col = ''
     for _ in range(5):
         row = list(input())
+        # 행의 최대 길이를 저장
         if len(row) > l:
             l = len(row)
         arr.append(row)
     for i in range(l):
         for j in range(5):
+            # 행의 첫번째 항목들을 꺼내면서 col 문자열에 추가, 더이상 꺼낼 글자가 없는 행은 조건문으로 skip
             if arr[j]:
-                col.append(arr[j].pop(0))
-    print(f'#{tc} {"".join(col)}')
+                col += arr[j].pop(0)
+    print('#{} {}'.format(tc, col))
