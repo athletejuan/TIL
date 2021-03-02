6571708e1f6f3765684ab336a7ed644b64af0c2e
athletejuan <vanillasky84.0627@gmail.com>
2021-03-03 08:53:46 +0900

    SWEA_PS_Queue_circulation

--- Algorithm/D2/5097.py
+++ Algorithm/D2/5097.py
@@ -1,6 +1,15 @@
 T = int(input())
 
-for test_case in range(1, T+1):
-    N, M = map(int, input().split())
-    arr = input().split()
-    print(f'#{test_case} {arr[M%N]}')
+for tc in range(1, T+1):
+    N,M = map(int, input().split())
+    queue = input().split()
+    for _ in range(M):
+        queue.append(queue.pop(0))
+    print('#{} {}'.format(tc, queue[0]))
+
+
+# 1st try
+# for test_case in range(1, T+1):
+#     N, M = map(int, input().split())
+#     arr = input().split()
+#     print(f'#{test_case} {arr[M%N]}')
