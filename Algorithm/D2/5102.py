fe8ed82b96f93e291aaec0fe4975e7dfb09c0e4e
athletejuan <vanillasky84.0627@gmail.com>
2021-03-07 10:38:00 +0900

    SWEA_PS_Queue_node_distance_live

--- Algorithm/D2/5102.py
+++ Algorithm/D2/5102.py
@@ -32,3 +32,32 @@ for tc in range(1, T+1):
     else:
         dis = 0
     print('#{} {}'.format(tc, dis))
+
+
+# live code
+# def BFS(S):
+#     Q = [[S, 0]]
+#     visited = [False] * (V+1)
+#     visited[S] = True
+
+#     while Q:
+#         v, dist = Q.pop(0)
+
+#         if v == G:
+#             return dist
+
+#         for i in range(1, V+1):
+#             if adj_arr[v][i] and not visited[i]:
+#                 Q.append([i, dist+1])
+#                 visited[i] = True
+#     return 0
+
+# for tc in range(1, T+1):
+#     V,E = map(int, input().split())
+#     adj_arr = [[0] * (V+1) for _ in range(V+1)]
+#     for _ in range(E):
+#         x,y = list(map(int, input().split()))
+#         adj_arr[x][y] = adj_arr[y][x] = 1
+#     S,G = map(int, input().split())
+
+#     print('#{} {}'.format(tc, BFS(S)))
