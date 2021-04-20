d93f97f0dbe2af446fcfed2d50073ccc1fc438ad
athletejuan <vanillasky84.0627@gmail.com>
2021-04-21 01:30:54 +0900

    SWEA_D4_number_continuation

---
+++ Algorithm/D4/2819.py
@@ -0,0 +1,23 @@
+T = int(input())
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+def DFS(x,y,n):
+    if len(n) > 6:
+        numbers.add(n)
+        return
+    for i in range(4):
+        a,b = x+dx[i],y+dy[i]
+        if 0 <= a < 4 and 0 <= b < 4:
+            n += base[a][b]
+            DFS(a,b,n)
+            n = n[:-1]
+
+for tc in range(1, T+1):
+    base = [input().split() for _ in range(4)]
+    numbers = set()
+
+    for i in range(4):
+        for j in range(4):
+            DFS(i,j,base[i][j])
+    print('#{} {}'.format(tc, len(numbers)))
