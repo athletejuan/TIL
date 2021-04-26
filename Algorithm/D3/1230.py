1b7b1029cf089d83a385f12d1f4c02feee6e74fe
athletejuan <vanillasky84.0627@gmail.com>
2021-04-27 00:45:15 +0900

    SWEA_D3_cryptogram3

---
+++ Algorithm/D3/1230.py
@@ -0,0 +1,18 @@
+for tc in range(1, 11):
+    N = int(input())
+    origin = list(input().split())
+    C = int(input())
+    command = list(input().split())
+    for i in range(len(command)):
+        if command[i] == 'I':
+            idx = int(command[i+1])
+            length = int(command[i+2])
+            origin = origin[:idx] + command[i+3:i+3+length] + origin[idx:]
+        elif command[i] == 'D':
+            idx = int(command[i+1])
+            length = int(command[i+2])
+            origin = origin[:idx] + origin[(idx+length):]
+        elif command[i] == 'A':
+            length = int(command[i+1])
+            origin += command[i+2:i+2+length]
+    print('#{} {}'.format(tc, ' '.join(origin[:10])))
