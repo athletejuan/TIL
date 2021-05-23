88b2f9d538e6763172a5c61246dc98311bcac707
athletejuan <vanillasky84.0627@gmail.com>
2021-05-24 00:46:50 +0900

    travel

---
+++ Algorithm/BOJ/9372.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+T = int(input())
+
+for _ in range(T):
+    N,M = map(int, input().split())
+    for i in range(M):
+        d,a = map(int, input().split())
+    print(N-1)
