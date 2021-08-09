e3e0a9273512cbb1efe6e659930f430f1c13da20
athletejuan <vanillasky84.0627@gmail.com>
2021-08-10 00:31:35 +0900

    SWEA_D3_box_change_recap

--- Algorithm/D3/5789.py
+++ Algorithm/D3/5789.py
@@ -1,18 +1,29 @@
 T = int(input())
 
-for test_case in range(1, T+1):
-    N, Q = map(int, input().split())
-    ranges = []
-    new = []
-    for i in range(Q):
-        ranges.append(list(map(int, input().split())))
-    for j in range(1, N+1):
-        num = 0
-        count = 0
-        for k in ranges[::-1]:
-            if k[0] <= j <= k[1]:
-                num = Q-count
-                break
-            count += 1
-        new.append(str(num))
-    print(f'#{test_case} {" ".join(new)}')
+# 1,798ms
+for tc in range(1, T+1):
+    N,Q = map(int, input().split())
+    boxes = ['0'] * (N+1)
+    for i in range(1, Q+1):
+        L,R = map(int, input().split())
+        for j in range(L, R+1):
+            boxes[j] = str(i)
+    print('#{} {}'.format(tc, ' '.join(boxes[1:])))
+
+# 1st try (847ms)
+# for test_case in range(1, T+1):
+#     N, Q = map(int, input().split())
+#     ranges = []
+#     new = []
+#     for i in range(Q):
+#         ranges.append(list(map(int, input().split())))
+#     for j in range(1, N+1):
+#         num = 0
+#         count = 0
+#         for k in ranges[::-1]:
+#             if k[0] <= j <= k[1]:
+#                 num = Q-count
+#                 break
+#             count += 1
+#         new.append(str(num))
+#     print(f'#{test_case} {" ".join(new)}')
