df093cacbdf0dc98cdf7570977d38f5fc3b53506
athletejuan <vanillasky84.0627@gmail.com>
2021-08-11 08:46:42 +0900

    SWEA_D3_part01_view_recap

--- Algorithm/D3/1206.py
+++ Algorithm/D3/1206.py
@@ -1,21 +1,40 @@
 for tc in range(1, 11):
-    N = int(input())
-    bds = list(map(int, input().split()))
+    L = int(input())
+    buildings = list(map(int, input().split()))
     view = 0
-    for i in range(2, N-2):
-        top, sec = 0, 0
-        for idx,bd in enumerate(bds[i-2:i+3]):
-            if bd > top:
-                top = bd
-                top_idx = idx
-        if top_idx == 2:
-            for idx,bd in enumerate(bds[i-2:i+3]):
-                if idx == 2:
-                    continue
-                elif bd > sec:
-                    sec = bd
-            view += (top - sec)
+
+    for i in range(2, L-2):
+        height = buildings[i]
+        temp_top = 0
+        for j in range(4):
+            idx = [-2, -1, 1, 2]
+            if buildings[i+idx[j]] > temp_top:
+                temp_top = buildings[i+idx[j]]
+
+        if height > temp_top:
+            view += height - temp_top
     print('#{} {}'.format(tc, view))
+
+
+# 3rd try
+# for tc in range(1, 11):
+#     N = int(input())
+#     bds = list(map(int, input().split()))
+#     view = 0
+#     for i in range(2, N-2):
+#         top, sec = 0, 0
+#         for idx,bd in enumerate(bds[i-2:i+3]):
+#             if bd > top:
+#                 top = bd
+#                 top_idx = idx
+#         if top_idx == 2:
+#             for idx,bd in enumerate(bds[i-2:i+3]):
+#                 if idx == 2:
+#                     continue
+#                 elif bd > sec:
+#                     sec = bd
+#             view += (top - sec)
+#     print('#{} {}'.format(tc, view))
 
 
 # 2nd try
