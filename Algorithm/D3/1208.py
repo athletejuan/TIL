a7223fe83efac539bd0d1f9790c238fc6325b661
athletejuan <vanillasky84.0627@gmail.com>
2021-08-12 00:28:30 +0900

    SWEA_D3_part01_flatten_counting_sort_recap

--- Algorithm/D3/1208.py
+++ Algorithm/D3/1208.py
@@ -1,27 +1,67 @@
 for tc in range(1, 11):
-    count = int(input())
+    dump_times = int(input())
     boxes = list(map(int, input().split()))
-    for i in range(count):
-        max_idx, min_idx = 0, 0
-        for idx, box in enumerate(boxes):
-            if not idx:
-                max_val, min_val = box, box
-            else:
-                if max_val < box:
-                    max_idx, max_val = idx, box
-                if min_val > box:
-                    min_idx, min_val = idx, box
-        boxes[max_idx] -= 1
-        boxes[min_idx] += 1
-    for j in range(100):
-        if not j:
-            top, bottom = boxes[0], boxes[0]
-        else:
-            if boxes[j] > top:
-                top = boxes[j] 
-            if boxes[j] < bottom:
-                bottom = boxes[j] 
-    print('#{} {}'.format(tc, top - bottom))
+
+    # 박스 높이의 분포를 카운팅하고, 이때 가장 높은 높이와 가장 낮은 높이를 찾는다.
+    heights_num = [0] * 101
+    max_height = 0
+    min_height = 100
+    for box in boxes:
+        heights_num[box] += 1
+        if max_height < box:
+            max_height = box
+        if min_height > box:
+            min_height = box
+
+    # 덤프를 실시한다. 가장 높은 높이와 가장 낮은 높이의 카운팅을 하나 차감해주고,
+    # 가장 낮은 높이보다 하나 큰 높이와 가장 높은 높이보다  하나 작은 높이의 카운팅을 하나 높인다.
+    for dump in range(dump_times):
+
+        # 더이상 덤프를 못하는 경우 break문으로 빠져 나와준다.
+        if max_height - min_height < 2:
+            result = max_height - min_height
+            break
+
+        heights_num[max_height] -= 1
+        heights_num[max_height - 1] += 1
+        heights_num[min_height] -= 1
+        heights_num[min_height + 1] += 1
+
+        # 가장 높은 높이와 가장 낮은 높이를 업데이트 한다
+        if not heights_num[max_height]:
+            max_height -= 1
+        if not heights_num[min_height]:
+            min_height += 1
+
+        result = max_height - min_height
+
+    print('#{} {}'.format(tc, result))
+
+
+# for tc in range(1, 11):
+#     count = int(input())
+#     boxes = list(map(int, input().split()))
+#     for i in range(count):
+#         max_idx = min_idx = 0
+#         for idx, box in enumerate(boxes):
+#             if not idx:
+#                 max_val = min_val = box
+#             else:
+#                 if max_val < box:
+#                     max_idx, max_val = idx, box
+#                 if min_val > box:
+#                     min_idx, min_val = idx, box
+#         boxes[max_idx] -= 1
+#         boxes[min_idx] += 1
+#     for j in range(100):
+#         if not j:
+#             top = bottom = boxes[0]
+#         else:
+#             if boxes[j] > top:
+#                 top = boxes[j] 
+#             if boxes[j] < bottom:
+#                 bottom = boxes[j] 
+#     print('#{} {}'.format(tc, top - bottom))
 
 
 
