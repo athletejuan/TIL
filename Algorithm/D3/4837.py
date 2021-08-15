9c7dd47ba178a65dc5fa128967096fe58983d71f
athletejuan <vanillasky84.0627@gmail.com>
2021-08-15 13:46:34 +0900

    SWEA_PS_LIST2_sum_of_subsets_recap

--- Algorithm/D3/4837.py
+++ Algorithm/D3/4837.py
@@ -3,15 +3,16 @@ T = int(input())
 arr = [i for i in range(1, 13)]
 for tc in range(1, T+1):
     N,K = map(int, input().split())
-    cnt = 0
-    for i in range(1 << 12):
-        sub = []
+    result = 0
+    for i in range(1<<12):
+        count = total = 0
         for j in range(12):
-            if i & (1 << j):
-                sub.append(arr[j])
-        if len(sub) == N and sum(sub) == K:
-            cnt += 1
-    print(f'#{tc} {cnt}')
+            if i & (1<<j):
+                count += 1
+                total += arr[j]
+        if count == N and total == K:
+            result += 1
+    print('#{} {}'.format(tc, result))
 
 
 # Tony
