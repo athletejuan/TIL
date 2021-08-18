8666d84fd42ee8fca2a8b47d076ca6d9d412124c
athletejuan <vanillasky84.0627@gmail.com>
2021-08-19 00:10:11 +0900

    SWEA_D4_part04_finding_a_way_recap

--- Algorithm/D4/1219.py
+++ Algorithm/D4/1219.py
@@ -1,29 +1,58 @@
-for test_case in range(1,11):
-    T,W = map(int, input().split())
-    ways = list(input().split())
-    wdict = {}
-    values = []
-    for _ in range(W):
-        if ways[0] in wdict:
-            wdict[ways.pop(0)].append(ways.pop(0))
-        else:
-            wdict[ways.pop(0)] = [ways.pop(1)]
-    nxt = wdict['0']
-    possible = True
-    impossible = True
-    while possible and impossible:
-        after = []
-        for i in range(len(nxt)):
-            if nxt[i] in wdict:
-                after.extend(wdict[nxt[i]])
-                if '99' in after:
-                    print(f'#{test_case} 1')
-                    possible = False
-                    break
-        else:
-            nxt = list(set(after))
-            if not after:
-                impossible = False
-                break
-    if not impossible:
-        print(f'#{test_case} 0')
+def dfs(s):
+    global result
+    if s == 99:
+        result = 1
+        return
+    for i in range(len(links[s])):
+        dfs(links[s][i])
+
+for tc in range(1, 11):
+    tc,E = map(int, input().split())
+    edges = list(map(int, input().split()))
+    links = [[] for _ in range(101)]
+    result = 0
+    for i in range(E):
+        links[edges[i*2]].append(edges[i*2+1])
+
+    for j in range(len(links[0])):
+        dfs(links[0][j])
+
+    print('#{} {}'.format(tc, result))
+
+
+# lecture code
+# def check_path(G):
+#     # 방문 정점
+#     visited = [0 for _ in range(100)]
+#     # 가장 첫번째 정점 넣어 놓고 시작
+#     stack = [0]
+
+#     # stack이 빌 때까지 (모든 정점 방문)
+#     while stack:
+#         # 특정 정점을 stack에서 꺼내서
+#         v = stack.pop()
+#         # 그 정점에 방문하지 않았다면
+#         if not visited[v]:
+#             # 방문처리하고
+#             visited[v] = 1
+#             # 인접 정점 찾아서 stack에 추가
+#             stack += G[v]
+
+#     # 최종적으로 99정점의 방문여부 return
+#     return visited[99]
+
+# T = 1
+# for _ in range(1, T+1):
+#     tc, E = input().split()
+#     G = [[] for _ in range(100)]
+#     V = list(map(int, input().split()))
+#     for idx in range(0, len(V), 2):
+#         # fr -> 정점
+#         fr = V[idx]
+#         # to -> 해당 정점에 연결된 값
+#         to = V[idx+1]
+#         # 인접 리스트 생성
+#         G[fr].append(to)
+#     print(G)
+#     result = check_path(G)
+#     print('#{} {}'.format(tc, result))
