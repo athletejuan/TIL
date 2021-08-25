349157a889e293c4b5e97c19f937a31621c9f174
athletejuan <vanillasky84.0627@gmail.com>
2021-08-26 00:33:24 +0900

    SWEA_PS_Queue_pizza_recap

--- Algorithm/D3/5099.py
+++ Algorithm/D3/5099.py
@@ -2,33 +2,23 @@ T = int(input())
 
 for tc in range(1, T+1):
     N,M = map(int, input().split())
-    Cheese = list(map(int, input().split()))
-    oven = [Cheese[i] for i in range(N)]
-    bake = [1] * N + [0] * (M-N)
-    pizza_num = [i for i in range(N)]
-    waiting = True
-    end = 0
+    pizza = list(map(int, input().split()))
+    q = [[] for _ in range(N)]
+    for i in range(N):
+        q[i] = [i+1, pizza[i]]
 
-    while end < N:
-        end = 0
-        for idx, cs in enumerate(oven):
-            if cs:
-                if oven[idx] == 1:
-                    last = pizza_num[idx]+1
-                oven[idx] = cs // 2
-                if not oven[idx]:
-                    if waiting:
-                        for i, wait in enumerate(bake):
-                            if not wait:
-                                pizza_num[idx] = i
-                                oven[idx] = Cheese[i]
-                                bake[i] = 1
-                                break
-                        else:
-                            waiting = False
-            else:
-                end += 1
-    print('#{} {}'.format(tc, last))
+    a = 0
+    while len(q) > 1:
+        idx, cheese = q.pop(0)
+        cheese //= 2
+        if cheese:
+            q.append([idx, cheese])
+        else:
+            if a < M-N:
+                q.append([N+a+1, pizza[N+a]])
+                a += 1
+
+    print('#{} {}'.format(tc, q[0][0]))
 
 
 # live code
@@ -52,3 +42,37 @@ for tc in range(1, T+1):
 #                 firepot.append([next_pizza+1, pizza[next_pizza]])
 #                 next_pizza += 1
 #     print('#{} {}'.format(tc, firepot[0][0]))
+
+
+# 1st try
+# T = int(input())
+#
+# for tc in range(1, T+1):
+#     N,M = map(int, input().split())
+#     Cheese = list(map(int, input().split()))
+#     oven = [Cheese[i] for i in range(N)]
+#     bake = [1] * N + [0] * (M-N)
+#     pizza_num = [i for i in range(N)]
+#     waiting = True
+#     end = 0
+#
+#     while end < N:
+#         end = 0
+#         for idx, cs in enumerate(oven):
+#             if cs:
+#                 if oven[idx] == 1:
+#                     last = pizza_num[idx]+1
+#                 oven[idx] = cs // 2
+#                 if not oven[idx]:
+#                     if waiting:
+#                         for i, wait in enumerate(bake):
+#                             if not wait:
+#                                 pizza_num[idx] = i
+#                                 oven[idx] = Cheese[i]
+#                                 bake[i] = 1
+#                                 break
+#                         else:
+#                             waiting = False
+#             else:
+#                 end += 1
+#     print('#{} {}'.format(tc, last))
