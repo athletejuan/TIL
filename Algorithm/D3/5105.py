2c3b1f2adcf384602089243b0bdaba01b2552b41
athletejuan <vanillasky84.0627@gmail.com>
2021-08-26 00:33:42 +0900

    SWEA_PS_Queue_maze_distance_recap

--- Algorithm/D3/5105.py
+++ Algorithm/D3/5105.py
@@ -3,39 +3,31 @@ dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
 def BFS():
-    global move
-    while way:
-        R = len(way)
-        move += 1
-        for _ in range(R):
-            x, y = way.pop(0)
-            for i in range(4):
-                a = x + dx[i]
-                b = y + dy[i]
-                maze[x][y] = '1'
-                if 0 <= a < N and 0 <= b < N:
-                    if maze[a][b] == '0':
-                        way.append([a, b])
-                    elif maze[a][b] == '2':
-                        move -= 1
-                        return
-            if not way:
-                move = 0
-                return
+    while move:
+        x, y = move.pop(0)
+        for i in range(4):
+            a = x + dx[i]
+            b = y + dy[i]
+            if 0 <= a < N and 0 <= b < N and not dist[a][b]:
+                if maze[a][b] == '0':
+                    move.append([a, b])
+                    dist[a][b] = dist[x][y] + 1
+                elif maze[a][b] == '3':
+                    return dist[x][y]
+    return 0
 
 for tc in range(1, T+1):
     N = int(input())
     maze = [list(input()) for _ in range(N)]
-    way = []
-    move = 0
+    move = []
+    dist = [[0]*N for _ in range(N)]
 
     for i in range(N):
         for j in range(N):
-            if maze[i][j] == '3':
-                way.append([i,j])
+            if maze[i][j] == '2':
+                move.append([i,j])
                 break
-    BFS()
-    print('#{} {}'.format(tc, move))
+    print('#{} {}'.format(tc, BFS()))
 
 
 # live code
@@ -81,3 +73,44 @@ for tc in range(1, T+1):
 
 #     S,G = search()
 #     print('#{} {}'.format(tc, BFS(S,G)))
+
+
+# 1st try
+# T = int(input())
+# dx = [1, -1, 0, 0]
+# dy = [0, 0, 1, -1]
+#
+# def BFS():
+#     global move
+#     while way:
+#         R = len(way)
+#         move += 1
+#         for _ in range(R):
+#             x, y = way.pop(0)
+#             for i in range(4):
+#                 a = x + dx[i]
+#                 b = y + dy[i]
+#                 maze[x][y] = '1'
+#                 if 0 <= a < N and 0 <= b < N:
+#                     if maze[a][b] == '0':
+#                         way.append([a, b])
+#                     elif maze[a][b] == '2':
+#                         move -= 1
+#                         return
+#             if not way:
+#                 move = 0
+#                 return
+#
+# for tc in range(1, T+1):
+#     N = int(input())
+#     maze = [list(input()) for _ in range(N)]
+#     way = []
+#     move = 0
+#
+#     for i in range(N):
+#         for j in range(N):
+#             if maze[i][j] == '3':
+#                 way.append([i,j])
+#                 break
+#     BFS()
+#     print('#{} {}'.format(tc, move))
