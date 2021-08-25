77950f152cc4306eee0f674cd956581c8bf5ea11
athletejuan <vanillasky84.0627@gmail.com>
2021-08-26 00:32:46 +0900

    SWEA_PS_Queue_node_distance_recap

--- Algorithm/D2/5102.py
+++ Algorithm/D2/5102.py
@@ -1,37 +1,27 @@
+def BFS(v):
+    q = [v]
+    visited[v] = 1
+    while q:
+        v = q.pop(0)
+        for w in range(1, V+1):
+            if adj[v][w] and not visited[w]:
+                q.append(w)
+                visited[w] = visited[v] + 1
+                if w == G:
+                    return visited[v]
+    return 0
+
 T = int(input())
-
-def BFS():
-    global fin
-    candidate = []
-    for node in nodes:
-        if links[node][G]:
-            fin = 1
-            return
-        else:
-            for j in range(1, V+1):
-                if links[node][j]:
-                    links[node][j] = 0
-                    candidate.append(j)
-    return candidate
-
 for tc in range(1, T+1):
     V,E = map(int, input().split())
-    links = [[0] * (V+1) for _ in range(V+1)]
-    for _ in range(E):
-        x,y = list(map(int, input().split()))
-        links[x][y] = links[y][x] = 1
+    adj = [[0] * (V+1) for _ in range(V+1)]
+    visited = [0] * (V+1)
+    for i in range(E):
+        x,y = map(int, input().split())
+        adj[x][y] = adj[y][x] = 1
     S,G = map(int, input().split())
 
-    nodes = [S]
-    dis, fin = 0, 0
-    while nodes:
-        nodes = BFS()
-        dis += 1
-        if fin:
-            break
-    else:
-        dis = 0
-    print('#{} {}'.format(tc, dis))
+    print('#{} {}'.format(tc, BFS(S)))
 
 
 # live code
@@ -61,3 +51,40 @@ for tc in range(1, T+1):
 #     S,G = map(int, input().split())
 
 #     print('#{} {}'.format(tc, BFS(S)))
+
+
+# 1st try
+# T = int(input())
+#
+# def BFS():
+#     global fin
+#     candidate = []
+#     for node in nodes:
+#         if links[node][G]:
+#             fin = 1
+#             return
+#         else:
+#             for j in range(1, V+1):
+#                 if links[node][j]:
+#                     links[node][j] = 0
+#                     candidate.append(j)
+#     return candidate
+#
+# for tc in range(1, T+1):
+#     V,E = map(int, input().split())
+#     links = [[0] * (V+1) for _ in range(V+1)]
+#     for _ in range(E):
+#         x,y = list(map(int, input().split()))
+#         links[x][y] = links[y][x] = 1
+#     S,G = map(int, input().split())
+#
+#     nodes = [S]
+#     dis, fin = 0, 0
+#     while nodes:
+#         nodes = BFS()
+#         dis += 1
+#         if fin:
+#             break
+#     else:
+#         dis = 0
+#     print('#{} {}'.format(tc, dis))
