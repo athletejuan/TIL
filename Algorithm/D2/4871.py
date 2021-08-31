2e498ade959da2226a35ad294e5a9785798a1e59
athletejuan <vanillasky84.0627@gmail.com>
2021-09-01 00:11:09 +0900

    SWEA_PS_Stack1_graph_route_recap

--- Algorithm/D2/4871.py
+++ Algorithm/D2/4871.py
@@ -1,20 +1,46 @@
 T = int(input())
 
-for test_case in range(1, T+1):
+def dfs(s):
+    if s == G:
+        return True
+    for i in range(1, V+1):
+        if adj[s][i]:
+            if dfs(i):
+                return True
+            # if i == G:
+            #     return True
+            # if dfs(i):
+            #     return True
+
+for tc in range(1, T+1):
     V, E = map(int, input().split())
-    M = []
-    for _ in range(E):
-        M.append(list(map(int, input().split())))
+    M = [list(map(int, input().split())) for _ in range(E)]
     S, G = map(int, input().split())
-    con = []
-    while S != G:
-        for m in M:
-            if S == m[0]:
-                con.append(m[1])
-        if con:
-            S = con.pop()
-        else:
-            print(f'#{test_case} 0')
-            break
-    else:
-        print(f'#{test_case} 1')
+
+    adj = [[0]*(V+1) for _ in range(V+1)]
+    for a,b in M:
+        adj[a][b] = 1
+
+    result = 1 if dfs(S) else 0
+    print('#{} {}'.format(tc, result))
+
+    # 반복문
+    # stack = [S]
+    # visited = [0] * (V+1)
+    # while stack:
+    #     # flag = False
+    #     start = stack.pop()
+    #     for i in range(1, V+1):
+    #         if adj[start][i]:
+    #             # if i == G:
+    #             #     print('#{} 1'.format(tc))
+    #             #     flag = True
+    #             #     break
+    #             visited[i] = 1
+    #             stack.append(i)
+    #     # if flag:
+    #     #     break
+    # # else:
+    # #     print('#{} 0'.format(tc))
+    # result = 1 if visited[G] else 0
+    # print('#{} {}'.format(tc, result))
