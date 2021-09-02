2fd1c042ad125eb6c54122fede94b31bf6528c9a
athletejuan <vanillasky84.0627@gmail.com>
2021-09-03 00:05:03 +0900

    SWEA_PS_Tree_binary_search

---
+++ Algorithm/D2/5176.py
@@ -0,0 +1,71 @@
+T = int(input())
+
+def inorder(n):
+    if n:
+        inorder(left[n])
+        tree.append(n)
+        inorder(right[n])
+
+for tc in range(1, T+1):
+    N = int(input())
+    left = [0] * (N+1)
+    right = [0] * (N+1)
+    tree = []
+
+    for i in range(1, N//2+1):
+        left[i] = i*2
+        if i*2+1 < N+1:
+            right[i] = i*2+1
+
+    inorder(1)
+    print('#{}'.format(tc), end=' ')
+    for idx, value in enumerate(tree):
+        if value == 1:
+            print(idx+1, end=' ')
+    for idx, value in enumerate(tree):
+        if value == N//2:
+            print(idx+1)
+
+
+# live
+# T = int(input())
+#
+# def in_order(v):
+#     global num
+#     if v <= N:
+#         in_order(v*2)
+#         tree[v] = num
+#         num += 1
+#         in_order(v*2+1)
+#
+# for tc in range(1, T+1):
+#     N = int(input())
+#     num = 1
+#
+#     tree = [0] * (N+1)
+#     in_order(1)
+#     print('#{} {} {}'.format(tc, tree[1], tree[N//2]))
+    
+
+# T = int(input())
+
+# class Node:
+#     def __init__(self, N):
+#         self.li = [0] * (N+1)
+#         self.cnt = 1
+#         self.numbering(1)
+
+#     def numbering(self, num):
+#         if num <= N:
+#             self.numbering(num*2)
+#             self.li[num] = self.cnt
+#             self.cnt += 1
+#             self.numbering(num*2+1)
+    
+#     def result(self):
+#         return ' '.join(map(str, (self.li[1], self.li[N//2])))
+
+# for tc in range(1, T+1):
+#     N = int(input())
+#     node = Node(N)
+#     print('#{} {}'.format(tc, node.result()))
