d2cc124bf1bdbcd1d4c71cff57decd1bf66f31d4
athletejuan <vanillasky84.0627@gmail.com>
2021-09-05 14:22:17 +0900

    SWEA_D3_part10_password_recap

--- Algorithm/D3/1234.py
+++ Algorithm/D3/1234.py
@@ -1,13 +1,30 @@
-for test_case in range(1, 11):
-    numbers = input().split()[1]
-    num_list = []
-    i = 0
-    for a in numbers:
-        num_list.append(a)
-    while i < len(num_list)-1:
-        if num_list[i] == num_list[i+1]:
-            num_list.pop(i)
-            num_list.pop(i)
-            i = -1
-        i += 1
-    print(f"#{test_case} {''.join(num_list)}")
+for tc in range(1, 11):
+    N,nums = input().split()
+    password = []
+    for i in range(int(N)):
+        if password and nums[i] == password[-1]:
+            password.pop()
+        else:
+            password.append(nums[i])
+        # if not password:
+        #     password.append(nums[i])
+        # else:
+        #     if nums[i] == password[-1]:
+        #         password.pop()
+        #     else:
+        #         password.append(nums[i])
+    print('#{} {}'.format(tc, ''.join(password)))
+
+# for test_case in range(1, 11):
+#     numbers = input().split()[1]
+#     num_list = []
+#     i = 0
+#     for a in numbers:
+#         num_list.append(a)
+#     while i < len(num_list)-1:
+#         if num_list[i] == num_list[i+1]:
+#             num_list.pop(i)
+#             num_list.pop(i)
+#             i = -1
+#         i += 1
+#     print(f"#{test_case} {''.join(num_list)}")
