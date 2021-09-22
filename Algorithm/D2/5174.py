877d76ab0a80b71be98574e5fe06ffacb875d086
athletejuan <vanillasky84.0627@gmail.com>
2021-09-23 01:27:47 +0900

    SWEA_PS_Tree_subtree_recap

--- Algorithm/D2/5174.py
+++ Algorithm/D2/5174.py
@@ -1,5 +1,32 @@
 T = int(input())
 
+def inorder(node):
+    global cnt
+    if node:
+        inorder(left[node])
+        cnt += 1
+        inorder(right[node])
+
+for tc in range(1, T+1):
+    E,N = map(int, input().split())
+    nodes = list(map(int, input().split()))
+    cnt = 0
+
+    left = [0] * (E+2)
+    right = [0] * (E+2)
+
+    for i in range(E):
+        if left[nodes[i*2]]:
+            right[nodes[i*2]] = nodes[i*2+1]
+        else:
+            left[nodes[i*2]] = nodes[i*2+1]
+    inorder(N)
+    print('#{} {}'.format(tc, cnt))
+
+
+# 2nd_try
+T = int(input())
+
 def sub_tree(node):
     global cnt
     if node[0]:
