d7025854004528d0a98a802b0ea4416d93bb813e
athletejuan <vanillasky84.0627@gmail.com>
2021-09-24 01:23:49 +0900

    SWEA_PS_Tree_binary_heap_recap

--- Algorithm/D2/5177.py
+++ Algorithm/D2/5177.py
@@ -1,9 +1,8 @@
 T = int(input())
 
 def heap(i):
-    while i//2:
-        if tree[i//2] > tree[i]:
-            tree[i//2],tree[i] = tree[i],tree[i//2]
+    while i//2 and tree[i//2] > tree[i]:
+        tree[i//2],tree[i] = tree[i],tree[i//2]
         i //= 2
 
 for tc in range(1, T+1):
@@ -12,10 +11,10 @@ for tc in range(1, T+1):
     nums = list(map(int, input().split()))
     total = 0
 
-    for i in range(N):
-        tree[i+1] = nums[i]
-        heap(i+1)
-        
+    for idx, num in enumerate(nums, start=1):
+        tree[idx] = num
+        heap(idx)
+
     while N:
         total += tree[N//2]
         N //= 2
