0300e4152f8302de2ddbcdb53fb10299597e9799
athletejuan <vanillasky84.0627@gmail.com>
2021-09-26 00:48:20 +0900

    SWEA_D4_part09_arithmetic_operation_recap

--- Algorithm/D4/1232.py
+++ Algorithm/D4/1232.py
@@ -2,14 +2,11 @@ def cal(op):
     if len(op) < 2:
         return int(op[0])
     else:
-        if op[0] == '+':
-            return cal(tree[int(op[1])]) + cal(tree[int(op[2])])
-        elif op[0] == '-':
-            return cal(tree[int(op[1])]) - cal(tree[int(op[2])])
-        elif op[0] == '*':
-            return cal(tree[int(op[1])]) * cal(tree[int(op[2])])
-        else:
-            return cal(tree[int(op[1])]) // cal(tree[int(op[2])])
+        L,R = cal(tree[int(op[1])]), cal(tree[int(op[2])])
+        if op[0] == '+': return L + R
+        elif op[0] == '-': return L - R
+        elif op[0] == '*': return L * R
+        else: return L // R
 
 for tc in range(1, 11):
     N = int(input())
@@ -19,3 +16,34 @@ for tc in range(1, 11):
         tree[int(node[0])] = node[1:]
 
     print('#{} {}'.format(tc, cal(tree[1])))
+
+
+# live
+def calc(v):
+    if len(tree[v]) == 2:
+        return tree[v][1]
+    else:
+        L = calc(tree[v][2])
+        R = calc(tree[v][3])
+
+        if tree[v][1] == '+': return L+R
+        elif tree[v][1] == '-': return L-R
+        elif tree[v][1] == '*': return L*R
+        else: return L/R
+
+for tc in range(1, 11):
+    N = int(input())
+    tree = [0] * (N+1)
+
+    for i in range(1, N+1):
+        tmp = input().split()
+
+        tree[int(tmp[0])] = tmp
+
+        if len(tmp) == 4:
+            tree[int(tmp[0])][2] = int(tree[int(tmp[0])][2])
+            tree[int(tmp[0])][2] = int(tree[int(tmp[0])][3])
+        else:
+            tree[int(tmp[0])][1] = int(tree[int(tmp[0])][1])
+
+    print('#{} {}'.format(tc, int(calc(1))))
