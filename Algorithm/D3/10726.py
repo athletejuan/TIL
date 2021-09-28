52e4083a6d8afb5c4bf32822e61d3e44ec8a3c69
athletejuan <vanillasky84.0627@gmail.com>
2021-09-28 18:30:45 +0900

    SWEA_D3_binary_expression

--- Algorithm/D3/10726.py
+++ Algorithm/D3/10726.py
@@ -1,15 +1,14 @@
 T = int(input())
 
-for test_case in range(1, T+1):
+for tc in range(1, T+1):
     N,M = map(int, input().split())
-    if M >= 2**N-1:
-        for _ in range(N):
-            re = M%2
-            if not re:
-                print(f'#{test_case} OFF')
-                break
-            M//=2
-        else:
-            print(f'#{test_case} ON')
-    else:
-        print(f'#{test_case} OFF')
+    status = 'OFF'
+    while M and N:
+        M,R = divmod(M,2)
+        if not R:
+            break
+        N -= 1
+    if not N:
+        status = 'ON'
+
+    print('#{} {}'.format(tc, status))
