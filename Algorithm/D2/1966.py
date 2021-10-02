9b56788453e89f0ebe498d6397a5467820ecf0fa
athletejuan <vanillasky84.0627@gmail.com>
2021-10-03 01:19:37 +0900

    SWEA_D2_sorted_numbers_recap

--- Algorithm/D2/1966.py
+++ Algorithm/D2/1966.py
@@ -1,6 +1,17 @@
 T = int(input())
 
-for test_case in range(1, T+1):
-    N, M = int(input()), list(map(int, input().split()))
-    ans = ' '.join(map(str, sorted(M)))
-    print(f'#{test_case} {ans}')
+def select_sort(arr):
+    for i in range(N-1):
+        min_idx = i
+        for j in range(i+1, N):
+            if arr[min_idx] > arr[j]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+
+for tc in range(1, T+1):
+    N = int(input()) 
+    arr = list(map(int, input().split()))
+    print('#{}'.format(tc), end=' ')
+    print(*select_sort(arr))
