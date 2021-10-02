61d13bb3970471390290e53411544150c07e0ce9
athletejuan <vanillasky84.0627@gmail.com>
2021-10-03 01:22:22 +0900

    SWEA_D3_part03_string

--- Algorithm/D3/1213.py
+++ Algorithm/D3/1213.py
@@ -1,10 +1,9 @@
-for t in range(1, 11):
-    test_case = input()
+for _ in range(1, 11):
+    tc = input()
     search = input()
     text = input()
     count = 0
     for i in range(len(text)-len(search)+1):
         if text[i:i+len(search)] == search:
             count += 1
-        print(text[i:i+len(search)])
-    print(f'#{test_case} {count}')
+    print('#{} {}'.format(tc, count))
