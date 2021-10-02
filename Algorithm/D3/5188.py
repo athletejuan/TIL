afb5b0f5d3aad9d498544f17bfc80832f12c43ca
athletejuan <vanillasky84.0627@gmail.com>
2021-10-03 01:27:36 +0900

    SWEA_PA_exhaustive_search_min_sum_recap

--- Algorithm/D3/5188.py
+++ Algorithm/D3/5188.py
@@ -4,21 +4,20 @@ dy = [1, 0]
 
 def DFS(x,y,now):
     global move
+    if x == y == N-1:
+        if move == base[0][0] or now < move:
+            move = now
+            return
+    if move != base[0][0] and now > move:
+        return
     for i in range(2):
-        a,b = x+dx[i],y+dy[i]
-        if 0 <= a < N and 0 <= b < N:
-            now += base[a][b]
-            DFS(a,b,now)
-            now -= base[a][b]
-            if move != base[0][0] and now > move:
-                break
-            if a == b == N-1:
-                if move == base[0][0] or now < move:
-                    move = now
+        nx, ny = x+dx[i], y+dy[i]
+        if 0 <= nx < N and 0 <= ny < N:
+            DFS(nx, ny, now + base[nx][ny])
 
 for tc in range(1, T+1):
     N = int(input())
     base = [list(map(int, input().split())) for _ in range(N)]
     move = base[0][0]
     DFS(0,0,move)
-    print('#{} {}'.format(tc, move+base[N-1][N-1]))
+    print('#{} {}'.format(tc, move))
