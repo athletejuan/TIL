f0e61052f3ad535a524f2e873d7043d2adac813f
athletejuan <vanillasky84.0627@gmail.com>
2021-10-06 00:24:03 +0900

    SWEA_PA_greedy_container_transportation

--- Algorithm/D3/5201.py
+++ Algorithm/D3/5201.py
@@ -2,14 +2,35 @@ T = int(input())
 
 for tc in range(1, T+1):
     N,M = map(int, input().split())
-    cons = sorted(list(map(int, input().split())), reverse=True)
-    trucks = sorted(list(map(int, input().split())))
+    containers = list(map(int, input().split()))
+    trucks = sorted(list(map(int, input().split())), reverse=True)
     total = 0
+    clist = [0] * 51
+    for i in range(N):
+        clist[containers[i]] += 1
 
-    while trucks:
-        big = trucks.pop()
-        for i in range(len(cons)):
-            if big >= cons[i]:
-                total += cons.pop(i)
-                break
+    for j in range(M):
+        limit = trucks[j]
+        while limit and not clist[limit]:
+            limit -= 1
+        clist[limit] -= 1
+        total += limit
     print('#{} {}'.format(tc, total))
+
+
+# 1st_try
+# T = int(input())
+#
+# for tc in range(1, T+1):
+#     N,M = map(int, input().split())
+#     cons = sorted(list(map(int, input().split())), reverse=True)
+#     trucks = sorted(list(map(int, input().split())))
+#     total = 0
+#
+#     while trucks:
+#         big = trucks.pop()
+#         for i in range(len(cons)):
+#             if big >= cons[i]:
+#                 total += cons.pop(i)
+#                 break
+#     print('#{} {}'.format(tc, total))
