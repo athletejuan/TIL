d6cd497e2900911f58c8d6900fe38e043b98d484
athletejuan <vanillasky84.0627@gmail.com>
2021-10-21 08:15:37 +0900

    printer_queue

--- Algorithm/BOJ/21_queue&dequeue/1966.py
+++ Algorithm/BOJ/21_queue&dequeue/1966.py
@@ -1,22 +1,21 @@
-T = int(input())
-
-for tc in range(T):
-    N,M = map(int, input().split())
-    Q = list(map(int, input().split()))
+def printer(M, Q):
     count = 0
-    breaker = False
-    for i in range(N):
+    while True:
         for idx, j in enumerate(Q):
             if j == max(Q):
                 count += 1
                 if M == idx:
-                    print(count)
-                    breaker = True
+                    return count
                 elif M > idx:
                     M -= (idx+1)
                 else:
                     M += len(Q[idx+1:])
                 Q = Q[idx+1:] + Q[:idx]
                 break
-        if breaker:
-            break
+
+
+T = int(input())
+for tc in range(T):
+    N,M = map(int, input().split())
+    Q = list(map(int, input().split()))
+    print(printer(M, Q))
